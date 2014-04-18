c220666845a2e9fd4ddf5648bdf9398f1a162477
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-04-18 00:25:40 -0400

    advanced_iterators: Added doctests to all the functions. Made minor changes to make sure the functions had reasonable behavior. Tested and they all passed.

--- dictionary_learning/advanced_iterators.py
+++ dictionary_learning/advanced_iterators.py
@@ -10,6 +10,29 @@ def index_generator(*sizes):
         
         Returns:
             chain_gen(generator):   a generator over every possible coordinated
+        
+        
+        Examples:
+            >>> index_generator(0) #doctest: +ELLIPSIS
+            <itertools.product object at 0x...>
+            
+            >>> list(index_generator(0))
+            []
+            
+            >>> list(index_generator(0, 2))
+            []
+            
+            >>> list(index_generator(2, 0))
+            []
+            
+            >>> list(index_generator(2, 1))
+            [(0, 0), (1, 0)]
+            
+            >>> list(index_generator(1, 2))
+            [(0, 0), (0, 1)]
+            
+            >>> list(index_generator(3, 2))
+            [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1)]
     """
     
     # Creates a list of xrange generator objects over each respective dimension of sizes
@@ -30,6 +53,16 @@ def list_indices_to_index_array(list_indices):
         
         Returns:
             chain_gen(tuple):       a tuple containing a numpy array in for each index
+            
+        Examples:
+            >>> list_indices_to_index_array([])
+            ()
+            
+            >>> list_indices_to_index_array([(1,2)])
+            (array([1]), array([2]))
+            
+            >>> list_indices_to_index_array([(1, 2), (5, 7), (33, 2)])
+            (array([ 1,  5, 33]), array([2, 7, 2]))
     """
     
     # Combines the indices so that one dimension is represented by each list.
@@ -48,12 +81,40 @@ def list_indices_to_numpy_bool_array(list_indices, shape):
         
         Returns:
             result(numpy.ndarray):   a numpy.ndarray with dtype bool (True for indices in list_indices and False otherwise).
+        
+        Examples:
+            >>> list_indices_to_numpy_bool_array([], ())
+            array(False, dtype=bool)
+            
+            >>> list_indices_to_numpy_bool_array([], (0))
+            array([], dtype=bool)
+            
+            >>> list_indices_to_numpy_bool_array([], (0,0))
+            array([], shape=(0, 0), dtype=bool)
+            
+            >>> list_indices_to_numpy_bool_array([], shape=(1))
+            array([False], dtype=bool)
+            
+            >>> list_indices_to_numpy_bool_array([(0,0)], (1,1))
+            array([[ True]], dtype=bool)
+            
+            >>> list_indices_to_numpy_bool_array([(2,3), (0,0), (0,2), (1,1)], (3,4))
+            array([[ True, False,  True, False],
+                   [False,  True, False, False],
+                   [False, False, False,  True]], dtype=bool)
+            
     """
     
     # Constructs the numpy.ndarray with False everywhere
     result = numpy.zeros(shape, dtype = bool)
     
+    # Gets the index array
+    # Done first to make sure that if list_indices is this [], or this (), or this [()]
+    # will be converted to this ().
+    index_array = list_indices_to_index_array(list_indices)
+    
     # Sets the given indices to True
-    result[list_indices_to_index_array(list_indices)] = True
+    if index_array != ():
+        result[index_array] = True
     
     return(result)
