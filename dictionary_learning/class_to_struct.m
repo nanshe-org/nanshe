aed2f355d42bc98decdeb5d3965bf1b05d3e300a
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-04-30 20:33:26 -0400

    class_to_struct: Added comments. Cleaned up the code a bit. Also, added classname parameter to classes being converted to provide the original class they were before being converted to a struct.

--- dictionary_learning/class_to_struct.m
+++ dictionary_learning/class_to_struct.m
@@ -1,11 +1,14 @@
 function [ new_struct_inst ] = class_to_struct ( new_class_inst )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%class_to_struct  Takes anything and turns any class objects it contains to
+%struct instances.
 
+    % If nothing else, we return the same thing we started with.
     new_struct_inst = new_class_inst;
     
-    
+    % We break groups of objects to deal with them separately.
     if (numel(new_struct_inst) > 1)
+        % Cells require {} for indexing. Otherwise, we don't extract their
+        % contents. Everything else is the same.
         if iscell(new_struct_inst)
             for i = 1:numel(new_struct_inst)
                 new_struct_inst{i} = class_to_struct(new_struct_inst{i});
@@ -15,14 +18,19 @@ function [ new_struct_inst ] = class_to_struct ( new_class_inst )
                 new_struct_inst(i) = class_to_struct(new_struct_inst(i));
             end
         end
+    % We take objects and structs and deal with them the same way.
     elseif isobject(new_struct_inst) | isstruct(new_struct_inst)
         if isobject(new_struct_inst)
+            % We save the classname for later so we can read it back if we
+            % want to.
+            old_classname = class(new_struct_inst);
             new_struct_inst = struct(new_struct_inst);
+            new_struct_inst.('classname') = old_classname;
         end
+        
+        % Take all fields and apply class_to_struct on them recursively.
         new_struct_inst_fields = fieldnames(new_struct_inst);
         for i = 1:numel(new_struct_inst_fields)
-            disp(inputname(1))
-            disp(new_struct_inst_fields{i})
             new_struct_inst.(new_struct_inst_fields{i}) = class_to_struct(new_struct_inst.(new_struct_inst_fields{i}));
         end
     end
