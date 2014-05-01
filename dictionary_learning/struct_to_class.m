cfdec296c15d2bca8b63a5fdfd0ad84bdfa513b2
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-05-01 15:28:10 -0400

    struct_to_class: Found references to class_to_struct replaced them with struct_to_class.

--- dictionary_learning/struct_to_class.m
+++ dictionary_learning/struct_to_class.m
@@ -1,6 +1,7 @@
 function [ new_class_inst ] = struct_to_class ( new_struct_inst )
 %struct_to_class  Takes a struct with a classname property and converts it
 % back to a class of the named type.
+% Has some issues on import of arrays. Need to revisit.
     
     % If nothing else, we return the same thing we started with.
     new_class_inst = new_struct_inst;
@@ -20,16 +21,16 @@ function [ new_class_inst ] = struct_to_class ( new_struct_inst )
         end
     % We take objects and structs and deal with them the same way.
     elseif isstruct(new_class_inst) & isfield(new_class_inst, 'classname')
-        % Take all fields and apply class_to_struct on them recursively.
+        % Take all fields and apply struct_to_class on them recursively.
         new_class_inst_fields = fieldnames(new_class_inst);
         for i = 1:numel(new_class_inst_fields)
-            new_class_inst.(new_class_inst_fields{i}) = class_to_struct(new_class_inst.(new_class_inst_fields{i}));
+            new_class_inst.(new_class_inst_fields{i}) = struct_to_class(new_class_inst.(new_class_inst_fields{i}));
         end
         
         % Grab the classname and drop it from the struct (maybe can drop
         % this).
-        classname = new_class_inst.classname
-        new_class_inst = rmfield(new_class_inst, 'classname')
+        classname = new_class_inst.classname;
+        new_class_inst = rmfield(new_class_inst, 'classname');
 
         % Simply calls the class with the struct.
         new_class_inst = eval([classname '(new_class_inst)']);
