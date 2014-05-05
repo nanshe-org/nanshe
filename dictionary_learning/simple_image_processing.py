c8234b771557d3f6bdf784ea4030dc58df948228
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-05-05 04:24:36 -0400

    simple_image_processing: Added options for in place calculations on numpy arrays. Also, added doctests for these cases.

--- dictionary_learning/simple_image_processing.py
+++ dictionary_learning/simple_image_processing.py
@@ -19,12 +19,13 @@ logger = advanced_debugging.logging.getLogger(__name__)
 
 
 @advanced_debugging.log_call(logger)
-def zeroed_mean_images(new_numpy_array):
+def zeroed_mean_images(new_numpy_array, in_place = False):
     """
         Takes and finds the mean for each image. Where each image is new_numpy_array[i] with some index i.
         
         Args:
             new_numpy_array(numpy.ndarray):     array images with time as the first index
+            in_place(bool):                     whether to preform the operation in place (changes new_numpy_array)
         
         Returns:
             result(numpy.ndarray):              The same array with each images mean removed. Where means[i] = mean(new_numpy_array[i])
@@ -54,7 +55,17 @@ def zeroed_mean_images(new_numpy_array):
             >>> import numpy; zeroed_mean_images(numpy.array([[1,2],[3,4]]))
             array([[-0.5,  0.5],
                    [-0.5,  0.5]])
+                   
+            >>> import numpy; a = numpy.array([[1.,2.],[3.,4.]]); a != zeroed_mean_images(a)
+            True
+                   
+            >>> import numpy; a = numpy.array([[1.,2.],[3.,4.]]); a == zeroed_mean_images(a, in_place = True)
+            True
     """
+    
+    result = new_numpy_array
+    if not in_place:
+        result = result.copy()
     
     # start with means having the same contents as the given images
     means = new_numpy_array
@@ -68,18 +79,19 @@ def zeroed_mean_images(new_numpy_array):
         means = means.reshape(means.shape + (1,))
     
     # broadcast and subtract the means so that the mean of all values in result[i] is zero
-    result = new_numpy_array - means
+    result -= means
     
     return(result)
 
 
 @advanced_debugging.log_call(logger)
-def renormalized_images(new_numpy_array, ord = 2):
+def renormalized_images(new_numpy_array, ord = 2, in_place = False):
     """
         Takes and finds the mean for each image. Where each image is new_numpy_array[i] with some index i.
         
         Args:
             new_numpy_array(numpy.ndarray):     array images with time as the first index
+            in_place(bool):                     whether to preform the operation in place (changes new_numpy_array)
         
         Returns:
             result(numpy.ndarray):              The same array with each images mean removed. Where means[i] = mean(new_numpy_array[i])
@@ -105,10 +117,17 @@ def renormalized_images(new_numpy_array, ord = 2):
             >>> import numpy; renormalized_images(numpy.array([[1.,2.],[3.,4.]]), ord = 1)
             array([[ 0.33333333,  0.66666667],
                    [ 0.42857143,  0.57142857]])
+                   
+            >>> import numpy; a = numpy.array([[1.,2.],[3.,4.]]); a != renormalized_images(a)
+            True
+                   
+            >>> import numpy; a = numpy.array([[1.,2.],[3.,4.]]); a == renormalized_images(a, in_place = True)
+            True
     """
     
-    # must copy as we will change contents
-    result = new_numpy_array.copy()
+    result = new_numpy_array
+    if not in_place:
+        result = result.copy()
     
     # take each image at each time turn the image into a vector and find the norm.
     # divide each image by this norm. (required for spams.trainDL)
