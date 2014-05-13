5c487cc66a5137a6df102694e427ae6e6e6d988e
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-05-13 02:52:26 +0200

    advanced_image_processing: Found that finding local maxima would not work in arbitrary dimensions. This has been corrected.

--- dictionary_learning/advanced_image_processing.py
+++ dictionary_learning/advanced_image_processing.py
@@ -271,8 +271,8 @@ def wavelet_denoising(new_image, **parameters):
     
     # Would be good to use peak_local_max as it has more features and is_local_maximum is removed in later versions,
     # but it requires skimage 0.8.0 minimum.
-    local_maxima_mask = skimage.morphology.is_local_maximum(new_wavelet_image_denoised, footprint = numpy.ones((3, 3)), labels = (new_wavelet_image_denoised > 0).astype(int))
-    #local_maxima = skimage.feature.peak_local_max(new_wavelet_image_denoised, footprint = numpy.ones((3, 3)), labels = (new_wavelet_image_denoised > 0).astype(int), indices = False)
+    local_maxima_mask = skimage.morphology.is_local_maximum(new_wavelet_image_denoised, footprint = numpy.ones((3,) * new_wavelet_image_denoised.ndim), labels = (new_wavelet_image_denoised > 0).astype(int))
+    #local_maxima = skimage.feature.peak_local_max(new_wavelet_image_denoised, footprint = numpy.ones((3,) * new_wavelet_image_denoised.ndim), labels = (new_wavelet_image_denoised > 0).astype(int), indices = False)
 
     # Group local maxima. Also, we don't care about differentiating them. If they are part of one
     local_maxima_labeled = label(local_maxima_mask)[0]
