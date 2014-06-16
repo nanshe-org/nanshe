9a862bc57a0cbac24c822004d04f89c29ec8f39d
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-06-16 07:49:41 -0400

    advanced_iterators: Added the function reformat_slices, which calls reformat_slice on multiple slices. Also, added the function len_slices, which calls len_slice on multiple slices.

--- nanshe/advanced_iterators.py
+++ nanshe/advanced_iterators.py
@@ -6,10 +6,8 @@ import math
 # Need in order to have logging information no matter what.
 import advanced_debugging
 
-
 # Get the logger
 logger = advanced_debugging.logging.getLogger(__name__)
-
 
 @advanced_debugging.log_call(logger)
 def index_generator(*sizes):
@@ -254,6 +252,7 @@ def cumulative_generator(new_op, new_iter):
         yield (cur)
 
 
+@advanced_debugging.log_call(logger)
 def reverse_each_element(new_iter):
     """
         Takes each element yielded by new_iter and reverses it using reversed.
@@ -284,6 +283,7 @@ def reverse_each_element(new_iter):
         yield ( type(each)(reversed(each)) )
 
 
+@advanced_debugging.log_call(logger)
 def filled_stringify_enumerate(new_list):
     """
         Takes each element yielded by new_iter and reverses it using reversed.
@@ -315,6 +315,7 @@ def filled_stringify_enumerate(new_list):
         yield ( (i, str(i).zfill(digits), each) )
 
 
+@advanced_debugging.log_call(logger, True)
 def reformat_slice(a_slice, a_length = None):
     """
         Takes a slice and reformats it to fill in as many undefined values as possible.
@@ -375,6 +376,7 @@ def reformat_slice(a_slice, a_length = None):
             >>> range(10)[reformat_slice(slice(2, None, 3), 10)] == range(10)[2::3]
             True
     """
+    assert a_slice != None, "err"
 
     new_slice_stop = a_slice.stop
     if new_slice_stop is None:
@@ -393,6 +395,32 @@ def reformat_slice(a_slice, a_length = None):
     return(new_slice)
 
 
+@advanced_debugging.log_call(logger, True)
+def reformat_slices(slices, lengths = None):
+    """
+        Takes a tuple of slices and reformats them to fill in as many undefined values as possible.
+
+        Args:
+            slices(tuple(slice)):        a tuple of slices to reformat.
+            lengths(tuple(int)):         a tuple of lengths to fill.
+
+        Returns:
+            (slice):                     a tuple of slices with all default values filled if possible.
+    """
+
+    new_lengths = lengths
+    if new_lengths is None:
+        new_lengths = [None] * len(slices)
+
+    new_slices = list(slices)
+    for i, each_length in enumerate(new_lengths):
+        new_slices[i] = reformat_slice(new_slices[i], each_length)
+
+    new_slices = tuple(new_slices)
+
+    return(new_slices)
+
+
 class UnknownSliceLengthException(Exception):
     """
         Raised if a slice does not have a known length.
@@ -401,6 +429,7 @@ class UnknownSliceLengthException(Exception):
     pass
 
 
+@advanced_debugging.log_call(logger, True)
 def len_slice(a_slice, a_length = None):
     """
         Determines how many elements a slice will contain.
@@ -464,3 +493,28 @@ def len_slice(a_slice, a_length = None):
     new_slice_size = int(math.ceil(float(new_slice_diff) / new_slice.step))
 
     return(new_slice_size)
+
+
+@advanced_debugging.log_call(logger, True)
+def len_slices(slices, lengths = None):
+    """
+        Takes a tuple of slices and reformats them to fill in as many undefined values as possible.
+
+        Args:
+            slices(tuple(slice)):        a tuple of slices to reformat.
+            lengths(tuple(int)):         a tuple of lengths to fill.
+
+        Returns:
+            (slice):                     a tuple of slices with all default values filled if possible.
+    """
+
+    new_slices = reformat_slices(slices, lengths)
+
+    lens = []
+
+    for each_slice in new_slices:
+        lens.append(len_slice(each_slice))
+
+    lens = tuple(lens)
+
+    return(lens)
