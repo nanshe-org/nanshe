1f2bb791d204693a48e74ee9f7c197c9f028e5e5
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-06-19 02:58:01 -0400

    HDF5_searchers: Provided a series of functions that make use of paths containing regex, which it uses to match several paths or possible paths in an HDF5 file.

---
+++ nanshe/HDF5_searchers.py
@@ -0,0 +1,152 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Jun 18, 2014 20:06:44 EDT$"
+
+
+import h5py
+import re
+import collections
+import itertools
+
+
+
+def get_matching_paths(a_filehandle, a_path_pattern):
+    """
+        Looks for existing paths that match the full provide pattern path.
+        Returns a list of matches for the given file handle.
+
+        Args:
+            a_filehandle(h5py.File):        an HDF5 file.
+            a_path_pattern(str):            an internal path (with patterns for each group) for the HDF5 file.
+
+        Note:
+            TODO: Write doctests.
+
+        Returns:
+            (list):                         a list of matching paths.
+    """
+
+    current_pattern_group_matches = []
+
+    if (isinstance(a_filehandle, h5py.Group) and a_path_pattern):
+        current_group = a_filehandle
+
+        a_path_pattern = a_path_pattern.strip("/")
+
+        to_split = a_path_pattern.find("/")
+        if to_split != -1:
+            current_path, next_path = a_path_pattern[:to_split], a_path_pattern[1 + to_split:]
+        else:
+            current_path, next_path = a_path_pattern, ""
+
+        current_pattern_group_regex = re.compile("/" + current_path + "/")
+
+        for each_group in current_group:
+            if current_pattern_group_regex.match("/" + each_group + "/") is not None:
+                next_group = current_group[each_group]
+
+                next_pattern_group_matches = get_matching_paths(next_group, next_path)
+
+                for each_next_pattern_group_match in next_pattern_group_matches:
+                    current_pattern_group_matches.append("/" + each_group + each_next_pattern_group_match)
+    else:
+        current_pattern_group_matches = [""]
+
+    return(current_pattern_group_matches)
+
+
+def get_matching_paths_groups(a_filehandle, a_path_pattern):
+    """
+        Looks for parts of the path pattern and tries to match them in order.
+        Returns a list of matches that can be combined to yield acceptable matches for the given file handle.
+
+        Note:
+            This works best when a tree structure is created systematically in HDF5. Then, this will recreate
+            what the tree structure could and may contain.
+
+        Args:
+            a_filehandle(h5py.File):        an HDF5 file.
+            a_path_pattern(str):            an internal path (with patterns for each group) for the HDF5 file.
+
+        Note:
+            TODO: Write doctests.
+
+        Returns:
+            (list):                         a list of matching paths.
+    """
+
+    def get_matching_paths_groups_recursive(a_filehandle, a_path_pattern):
+        current_pattern_group_matches = []
+
+        if (isinstance(a_filehandle, h5py.Group) and a_path_pattern):
+            current_pattern_group_matches.append(collections.OrderedDict())
+
+            current_group = a_filehandle
+
+            a_path_pattern = a_path_pattern.strip("\b").strip("/")
+
+            to_split = a_path_pattern.find("/")
+            if to_split != -1:
+                current_path, next_path = a_path_pattern[:to_split], a_path_pattern[1 + to_split:]
+            else:
+                current_path, next_path = a_path_pattern, ""
+
+            current_pattern_group_regex = re.compile("/" + current_path + "/")
+
+            for each_group in current_group:
+                if current_pattern_group_regex.match("/" + each_group + "/") is not None:
+                    next_group = current_group[each_group]
+
+                    next_pattern_group_matches = get_matching_paths_groups_recursive(next_group, next_path)
+
+                    current_pattern_group_matches[0][each_group] = None
+
+                    while ( len(current_pattern_group_matches) - 1 ) < len(next_pattern_group_matches):
+                        current_pattern_group_matches.append(collections.OrderedDict())
+
+                    for i, each_next_pattern_group_matches in enumerate(next_pattern_group_matches, start = 1):
+                        for each_next_pattern_group_match in each_next_pattern_group_matches:
+                            current_pattern_group_matches[i][each_next_pattern_group_match] = None
+        else:
+            current_pattern_group_matches = []
+
+        return(current_pattern_group_matches)
+
+    groups = get_matching_paths_groups_recursive(a_filehandle, a_path_pattern)
+
+    new_groups = []
+    for i in xrange(len(groups)):
+        new_groups.append(list(groups[i]))
+
+    groups = new_groups
+
+    return(groups)
+
+
+def get_matching_paths_groups_found(a_filehandle, a_path_pattern):
+    """
+        Looks for existing paths that match the full provide pattern path.
+        Returns a list of matches as keys and whether they are found in the HDF5 file or not.
+
+        Args:
+            a_filehandle(h5py.File):        an HDF5 file.
+            a_path_pattern(str):            an internal path (with patterns for each group) for the HDF5 file.
+
+        Note:
+            TODO: Write doctests.
+
+        Returns:
+            (collections.OrderedDict):      an ordered dictionary with possible paths that fit the pattern and whether they are found.
+    """
+
+    paths_found = collections.OrderedDict()
+
+    for each_path_components in itertools.product(*get_matching_paths_groups(a_filehandle, a_path_pattern)):
+        each_path = "/" + "/".join([_ for _ in each_path_components])
+
+        paths_found[each_path] = (each_path in a_filehandle)
+
+    return(paths_found)
