5a1bc696ec41340910df38046e0549198fccbaa4
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-06-20 15:57:45 -0400

    __init__: Segmentation fault will cause batch_learner to die when running SPAMS, but does not occur when neuron_volumina_viewer is not imported into nanshe. So, this has been commented. This is a work around for issue #33, which contains more details.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -6,10 +6,8 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 8:37:08PM$"
 
 __all__ = ["..", "advanced_debugging", "advanced_image_processing", "advanced_iterators", "advanced_numpy",
-           "batch_learner", "denoising", "HDF5_logger", "HDF5_searchers", "HDF5_serializers",
-           "neuron_matplotlib_viewer", "neuron_volumina_viewer", "pathHelpers", "read_config",
-           "simple_image_processing", "wavelet_transform"]
-
+           "batch_learner", "denoising", "HDF5_logger", "HDF5_searchers", "HDF5_serializers", #"neuron_volumina_viewer",
+           "pathHelpers", "read_config", "simple_image_processing", "wavelet_transform"]
 
 import advanced_debugging
 import advanced_image_processing
@@ -21,7 +19,7 @@ import HDF5_logger
 import HDF5_searchers
 import HDF5_serializers
 import neuron_matplotlib_viewer
-import neuron_volumina_viewer
+# import neuron_volumina_viewer    # Must be commented as there is some segfault coming from Volumina.
 import pathHelpers
 import read_config
 import simple_image_processing
