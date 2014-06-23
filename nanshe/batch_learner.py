6a83aaa558be33834d1661e9fd7b463e30bce685
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-06-23 00:40:52 -0400

    advanced_image_processing: Removed generate_neurons in favor of merging content with generate_save_neurons. batch_learner: Changed generate_save_neurons to contain the contents of generate_save_neurons.

--- nanshe/batch_learner.py
+++ nanshe/batch_learner.py
@@ -149,30 +149,48 @@ def generate_save_neurons(new_filename, debug = False, resume = False, **paramet
         if "original_images" not in new_file[output_directory]:
             new_file[output_directory]["original_images"] = new_file[new_hdf5_filepath_details.internalPath]
 
-        # Save them
-        if ("neurons" not in new_file[output_directory]):
-            # Copy out images for manipulation in memory
-            new_images = new_file[output_directory]["original_images"][:]
+        # Copy out images for manipulation in memory
+        new_images = new_file[output_directory]["original_images"][:]
 
-            # Get a debug logger for the HDF5 file (if needed)
-            array_debug_logger = HDF5_logger.generate_HDF5_array_logger(new_file[output_directory],
+        # Get a debug logger for the HDF5 file (if needed)
+        array_debug_logger = HDF5_logger.generate_HDF5_array_logger(new_file[output_directory],
                                                                         group_name = "debug",
                                                                         enable = debug,
                                                                         overwrite_group = True)
 
-            resume_logger = HDF5_logger.generate_HDF5_array_logger(new_file[output_directory])
+        # Saves intermediate result to make resuming easier
+        resume_logger = HDF5_logger.generate_HDF5_array_logger(new_file[output_directory])
 
-            if "original_images_max_projection" not in array_debug_logger:
-                array_debug_logger("original_images_max_projection", new_images.max(axis = 0))
+        # Preprocess images
+        new_preprocessed_images = None
+        if "preprocessed_images" in resume_logger:
+            new_preprocessed_images = resume_logger["preprocessed_images"]
+        else:
+            new_preprocessed_images = advanced_image_processing.preprocess_data(new_images, array_debug_logger = array_debug_logger, **parameters["preprocess_data"])
+            resume_logger("preprocessed_images", new_preprocessed_images)
+            array_debug_logger("preprocessed_images_max_projection", new_preprocessed_images.max(axis = 0))
 
-            # Generate the new neurons
-            new_neurons = advanced_image_processing.generate_neurons(new_images,
-                                                                     resume_logger = resume_logger,
-                                                                     array_debug_logger = array_debug_logger,
-                                                                     **parameters)
+        # Find the dictionary
+        new_dictionary = None
+        if "dictionary" in resume_logger:
+            new_dictionary = resume_logger["dictionary"]
+        else:
+            new_dictionary = advanced_image_processing.generate_dictionary(new_preprocessed_images, array_debug_logger = array_debug_logger, **parameters["generate_dictionary"])
+            resume_logger("dictionary", new_dictionary)
+            array_debug_logger("dictionary_max_projection", new_dictionary.max(axis = 0))
 
+        # Find the neurons
+        new_neurons = None
+        if "neurons" in resume_logger:
+            new_neurons = resume_logger["neurons"]
+        else:
+            new_neurons = advanced_image_processing.postprocess_data(new_dictionary, array_debug_logger, **parameters["postprocess_data"])
+            resume_logger("neurons", new_neurons)
+            if new_neurons.size == 0:
+                logger.warning("No neurons were found in the data.")
+
+        # Save the configuration parameters in the attributes as a string.
         if "parameters" not in new_file[output_directory].attrs:
-            # Write the configuration parameters in the attributes as a string.
             new_file[output_directory].attrs["parameters"] = repr(parameters)
 
 
