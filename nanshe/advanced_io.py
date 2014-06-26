2e40836860ae8bc3b52d69cc434995382438bdd0
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-06-26 18:35:24 -0400

    advanced_io: Added the function expand_pathname_list, which takes pathnames and uses regex to expand them into an explicit list of pathnames that match.

--- nanshe/advanced_io.py
+++ nanshe/advanced_io.py
@@ -2,3 +2,35 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Jun 26, 2014 18:24:25 EDT$"
 
 
+import glob
+
+import advanced_debugging
+
+
+
+# Get the logger
+logger = advanced_debugging.logging.getLogger(__name__)
+
+
+
+@advanced_debugging.log_call(logger)
+def expand_pathname_list(*pathnames):
+    """
+        Takes each pathname in those given and expands them using regex.
+
+        Args:
+            *pathnames(str):     pathnames to use regex on to expand.
+
+        Returns:
+            list:                a list of path names (without regex)
+
+    """
+
+    expanded_pathnames = []
+
+    # Completes any regex
+    expanded_pathnames = []
+    for each_pathname in pathnames:
+        expanded_pathnames.extend(glob.glob(each_pathname))
+
+    return(expanded_pathnames)
