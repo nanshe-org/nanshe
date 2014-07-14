9841c5d6bd8daafda380fe1f09feb143606898e9
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-07-14 14:40:35 -0400

    batch_learner: Renamed to nanshe_learner. neuron_volumina_viewer: Renamed to nanshe_viewer.
    nanshe_learner: Renamed from batch_learner.
    nanshe_viewer: Renamed from neuron_volumina_viewer.
    
    __init__: Renamed batch_learner to nanshe_learner and neuron_volumina_viewer to nanshe_viewer.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -6,20 +6,20 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 8:37:08PM$"
 
 __all__ = ["..", "debugging_tools", "advanced_image_processing", "additional_generators", "expanded_numpy",
-           "batch_learner", "denoising", "HDF5_recorder", "HDF5_searchers", "HDF5_serializers", #"neuron_volumina_viewer",
+           "nanshe_learner", "denoising", "HDF5_recorder", "HDF5_searchers", "HDF5_serializers", #"nanshe_viewer",
            "pathHelpers", "read_config", "simple_image_processing", "wavelet_transform"]
 
 import debugging_tools
 import advanced_image_processing
 import additional_generators
 import expanded_numpy
-import batch_learner
+import nanshe_learner
 import denoising
 import HDF5_recorder
 import HDF5_searchers
 import HDF5_serializers
 import neuron_matplotlib_viewer
-# import neuron_volumina_viewer    # Must be commented as there is some segfault coming from Volumina.
+# import nanshe_viewer    # Must be commented as there is some segfault coming from Volumina.
 import pathHelpers
 import read_config
 import simple_image_processing
