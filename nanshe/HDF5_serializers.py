2a8a7fe5e1fde8d6f0bb485593fd92b8d97cac0d
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-07-18 14:40:49 -0400

    HDF5_serializers: Changed read_numpy_structured_array_from_HDF5 so that it now properly reads h5py.Reference and h5py.RegionReference objects (provides support for external files as well using an attribute).

--- nanshe/HDF5_serializers.py
+++ nanshe/HDF5_serializers.py
@@ -5,6 +5,7 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$May 14, 2014 10:19:59PM$"
 
+
 import numpy
 import h5py
 
@@ -61,18 +62,18 @@ def read_numpy_structured_array_from_HDF5(file_handle, internalPath):
     """
         Serializes a NumPy structure array from an HDF5 file by using the HDF5 compound data type.
         Also, it will handle normal NumPy arrays and scalars, as well.
-        
+
         Note:
             HDF5 does not support generic Python objects. So, serialization of objects to something
             else (perhaps strs of fixed size) must be performed first.
-        
+
         Args:
             file_handle(HDF5 file):     either an HDF5 file or an HDF5 filename.
             internalPath(str):          an internal path for the HDF5 file.
-        
+
         Note:
             TODO: Write doctests.
-        
+
         Returns:
             data(numpy.ndarray):    the NumPy structure array.
     """
@@ -83,7 +84,31 @@ def read_numpy_structured_array_from_HDF5(file_handle, internalPath):
         file_handle = h5py.File(file_handle, "r")
         close_file_handle = True
 
-    data = file_handle[internalPath].value
+    data = None
+
+    data_object = file_handle[internalPath]
+    # data_file_handle may be different than file_handle if data_object is an ExternalLink.
+    data_file_handle = data_object.file
+    data_ref = data_object.value
+
+    if isinstance(data_ref, numpy.ndarray):
+        data = data_object.value
+    elif isinstance(data_ref, h5py.Reference):
+        if ("filename" in data_object.attrs) and (data_object.attrs["filename"] != file_handle.filename):
+            with h5py.File(data_object.attrs["filename"], "r") as external_file_handle:
+                if isinstance(data_ref, h5py.RegionReference):
+                    data = external_file_handle[data_ref][data_ref]
+                else:
+                    data = external_file_handle[data_ref].value
+        else:
+            if isinstance(data_ref, h5py.RegionReference):
+                    data = data_file_handle[data_ref][data_ref]
+            else:
+                data = data_file_handle[data_ref].value
+
+    # If data_object was an ExternalLink, then this is another file and it must be closed.
+    if data_file_handle != file_handle:
+        data_file_handle.close()
 
     if close_file_handle:
         file_handle.close()
