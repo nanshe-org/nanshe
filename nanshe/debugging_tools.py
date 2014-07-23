6e05c36ed6fae900595bcd8200f2f3818967b895
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-07-23 18:58:49 -0400

    debugging_tools: Changed to use tools from generic_decorators make code shorter and more flexible.

--- nanshe/debugging_tools.py
+++ nanshe/debugging_tools.py
@@ -8,9 +8,11 @@ __date__ = "$Apr 14, 2014 5:42:07PM$"
 
 import logging
 import traceback
-import functools
 import os
 import sys
+
+
+import generic_decorators
 
 
 # Nothing fancy. Just the basic logging unless otherwise specified, in which case this does nothing.
@@ -32,9 +34,12 @@ def log_call(logger, to_log_call = True, to_print_args = False, to_print_excepti
             to_print_args  (bool):         Whether to output the arguments and keyword arguments passed to the function.
                                            This should not automatically be true as some arguments may not be printable
                                            or may be expensive to print. Thus, it should be up to the developer to use
-                                           their own discretion. Further, we don't want to break their existing code.
+                                           their own discretion. Further, we don't want to break their existing code. It
+                                           will be stored as a global variable on the function, which can be changed at
+                                           runtime.
 
-            to_print_exception  (bool):    Whether to print the traceback when an exception is raise.
+            to_print_exception  (bool):    Whether to print the traceback when an exception is raise. It will be stored
+                                           as a global variable on the function, which can be changed at runtime.
         
         Returns:
             log_call_decorator (for wrapping)
@@ -51,10 +56,13 @@ def log_call(logger, to_log_call = True, to_print_args = False, to_print_excepti
                 log_call_callable_wrapped, which is wrapped around the function in question.
         """
 
-        @functools.wraps(callable)
+        @generic_decorators.wraps(callable)
+        @generic_decorators.static_variables(to_log_call = to_log_call,
+                                             to_print_args = to_print_args,
+                                             to_print_exception = to_print_exception)
         def log_call_callable_wrapped(*args, **kwargs):
             """
-                This is what will replace the orignal callable. It will behave the same except it will now log its
+                This is what will replace the original callable. It will behave the same except it will now log its
                 entrance and exit. If set, it will log its parameters too.
 
                 Args:
@@ -99,21 +107,6 @@ def log_call(logger, to_log_call = True, to_print_args = False, to_print_excepti
 
             # Return the result even None.
             return(result)
-
-        # Store the underlying callable. Automatic in Python 3.
-        log_call_callable_wrapped.__wrapped__ = callable
-
-        # Copy over the value of to_log_call for later use.
-        # Must be defined afterwards as functools.wraps will not copy it over to the wrapped instance.
-        log_call_callable_wrapped.to_log_call = to_log_call
-
-        # Copy over the value of to_print_args for later use.
-        # Must be defined afterwards as functools.wraps will not copy it over to the wrapped instance.
-        log_call_callable_wrapped.to_print_args = to_print_args
-
-        # Copy over the value of to_print_exception for later use.
-        # Must be defined afterwards as functools.wraps will not copy it over to the wrapped instance.
-        log_call_callable_wrapped.to_print_exception = to_print_exception
 
         # The callable wrapped.
         return(log_call_callable_wrapped)
