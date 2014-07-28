b044d6cc988af57281d9069ac04c4b292cdc7846
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-07-28 14:34:17 -0400

    test_HDF5_searchers: Added a test for HDF5_searchers.get_matching_paths_groups. This fixes #77.

--- tests/test_HDF5_searchers.py
+++ tests/test_HDF5_searchers.py
@@ -34,6 +34,23 @@ class TestHDF5Searchers(object):
                 if not ((_ == TestHDF5Searchers.groups_0[0]) and (__ == TestHDF5Searchers.groups_1[0])):
                     yield( u"/" + u"/".join([u"test", _, u"group", __, u"data"]) )
 
+    @staticmethod
+    def get_matching_paths_groups_generator():
+        yield([u"test"])
+        yield(TestHDF5Searchers.groups_0)
+        yield([u"group"])
+        yield(TestHDF5Searchers.groups_1)
+        yield([u"data"])
+
+    @staticmethod
+    def match_path_groups_gen(group_matches):
+        for _ in itertools.product(*group_matches):
+            yield( u"/" + u"/".join(_) )
+
+    @staticmethod
+    def get_matching_grouped_paths_gen():
+        return(TestHDF5Searchers.match_path_groups_gen(list(TestHDF5Searchers.get_matching_paths_groups_generator())))
+
     def setup(self):
         self.temp_dir = tempfile.mkdtemp()
 
@@ -48,6 +65,21 @@ class TestHDF5Searchers(object):
         assert(len(all_matched) == (len(TestHDF5Searchers.groups_0) * len(TestHDF5Searchers.groups_1) - 1))
 
         for _1, _2 in itertools.izip(TestHDF5Searchers.get_matching_paths_generator(), all_matched):
+            assert(_1 == _2)
+
+    def test_get_matching_paths_groups(self):
+        all_matched = nanshe.HDF5_searchers.get_matching_paths_groups(self.temp_hdf5_file, u"/test/[0-9]{3}/group/[0-9]/data")
+
+        num_permutations = 1
+        for each_group_match_list in all_matched:
+            num_permutations *= len(each_group_match_list)
+
+        assert(num_permutations == (len(TestHDF5Searchers.groups_0) * len(TestHDF5Searchers.groups_1)))
+
+        for _1, _2 in itertools.izip(TestHDF5Searchers.get_matching_paths_groups_generator(), all_matched):
+            assert(_1 == _2)
+
+        for _1, _2 in itertools.izip(TestHDF5Searchers.get_matching_grouped_paths_gen(), TestHDF5Searchers.match_path_groups_gen(all_matched)):
             assert(_1 == _2)
 
     def teardown(self):
