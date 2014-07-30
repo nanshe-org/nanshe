986cf8eedf7185c3a3600e9b4e056acb20360c2a
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-07-30 12:38:19 -0400

    test_HDF5_serializers: Added tests for read_numpy_structured_array_from_HDF5 from HDF5_serializers. This fixes #82.

--- tests/test_HDF5_serializers.py
+++ tests/test_HDF5_serializers.py
@@ -96,8 +96,74 @@ class TestHDF5Searchers(object):
         assert((data2 == self.temp_hdf5_file["data"].value).all())
 
 
-    def test_read_numpy_structured_array_from_HDF5(self):
-        pass
+    def test_read_numpy_structured_array_from_HDF5_1(self):
+        data1 = numpy.random.random((10, 10))
+
+        nanshe.HDF5_serializers.create_numpy_structured_array_in_HDF5(self.temp_hdf5_file, "data", data1)
+
+        data2 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file, "data")
+
+        assert(data1.dtype == data2.dtype)
+        assert(data1.shape == data2.shape)
+        assert((data1 == data2).all())
+
+        self.temp_hdf5_file["data_ref"] = self.temp_hdf5_file["data"].ref
+
+        data3 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file, "data_ref")
+
+        assert(data1.dtype == data3.dtype)
+        assert(data1.shape == data3.shape)
+        assert((data1 == data3).all())
+
+        self.temp_hdf5_file["data_rref"] = self.temp_hdf5_file["data"].regionref[2:8, 2:8]
+
+        data4 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file, "data_rref")
+
+        assert(data1[2:8, 2:8].dtype == data4.dtype)
+        assert(data1[2:8, 2:8].shape == data4.shape)
+        assert((data1[2:8, 2:8] == data4).all())
+
+        self.temp_hdf5_file2 = h5py.File(os.path.join(self.temp_dir, "test2.h5"), "w")
+
+        self.temp_hdf5_file2["data_lref"] = self.temp_hdf5_file["data"].ref
+        self.temp_hdf5_file2["data_lref"].attrs["filename"] = self.temp_hdf5_file.filename
+
+        data5 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file2, "data_lref")
+
+        assert(data1.dtype == data5.dtype)
+        assert(data1.shape == data5.shape)
+        assert((data1 == data5).all())
+
+        self.temp_hdf5_file2["data_lrref"] = self.temp_hdf5_file["data"].regionref[2:8, 2:8]
+        self.temp_hdf5_file2["data_lrref"].attrs["filename"] = self.temp_hdf5_file.filename
+
+        data6 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file2, "data_lrref")
+
+        assert(data1[2:8, 2:8].dtype == data6.dtype)
+        assert(data1[2:8, 2:8].shape == data6.shape)
+        assert((data1[2:8, 2:8] == data6).all())
+
+
+    def test_read_numpy_structured_array_from_HDF5_2(self):
+        data1 = numpy.zeros((10, 10), dtype=[("a", float, 2), ("b", int, 3)])
+        data1["a"] = numpy.random.random((10, 10, 2))
+        data1["b"] = numpy.random.random_integers(0, 10, (10, 10, 3))
+
+        nanshe.HDF5_serializers.create_numpy_structured_array_in_HDF5(self.temp_hdf5_file, "data", data1)
+
+        data2 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file, "data")
+
+        assert(data1.dtype == data2.dtype)
+        assert(data1.shape == data2.shape)
+        assert((data1 == data2).all())
+
+        self.temp_hdf5_file["data_ref"] = self.temp_hdf5_file["data"].ref
+
+        data3 = nanshe.HDF5_serializers.read_numpy_structured_array_from_HDF5(self.temp_hdf5_file, "data_ref")
+
+        assert(data1.dtype == data3.dtype)
+        assert(data1.shape == data3.shape)
+        assert((data1 == data3).all())
 
 
     def teardown(self):
