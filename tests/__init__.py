07b95e4fe34b4df0b3f91bc6819c213e65bcff34
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-04 14:49:08 -0400

    test_tiff_file_format: Added for testing the module tiff_file_format. This will address issues #125, #126, #127, and #128. __init__: Added test_tiff_file_format to __all__ and imported it as well.

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@
 __author__="John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ ="$Jun 6, 2014 4:21:22PM$"
 
-__all__ = ["..", "synthetic_data", "test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_HDF5_searchers", "test_HDF5_serializers", "random_dictionary_learning_data"]
+__all__ = ["..", "synthetic_data", "test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_HDF5_searchers", "test_HDF5_serializers", "test_tiff_file_format", "random_dictionary_learning_data"]
 
 
 import synthetic_data
@@ -14,4 +14,5 @@ import test_advanced_image_processing
 import test_debugging_tools
 import test_HDF5_searchers
 import test_HDF5_serializers
+import test_tiff_file_format
 import random_dictionary_learning_data
---
+++ tests/test_tiff_file_format.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Aug 04, 2014 14:48:56 EDT$"
+
+
