35d54a6c2eaa149eef5d0319e9373ff2adc3ce9c
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-05 14:03:33 -0400

    test_extended_region_props: Added for testing the class ExtendedRegionProps in the module advanced_image_processing. This will address issue #110. __init__: Added test_extended_region_props to __all__ and imported it as well.

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,13 +5,14 @@
 __author__="John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ ="$Jun 6, 2014 4:21:22PM$"
 
-__all__ = ["..", "synthetic_data", "test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_HDF5_searchers", "test_HDF5_serializers", "test_tiff_file_format", "random_dictionary_learning_data"]
+__all__ = ["..", "synthetic_data", "test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_extended_region_props", "test_HDF5_searchers", "test_HDF5_serializers", "test_tiff_file_format", "random_dictionary_learning_data"]
 
 
 import synthetic_data
 import test_additional_io
 import test_advanced_image_processing
 import test_debugging_tools
+import test_extended_region_props
 import test_HDF5_searchers
 import test_HDF5_serializers
 import test_tiff_file_format
---
+++ tests/test_extended_region_props.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Aug 05, 2014 14:02:24 EDT$"
+
+
