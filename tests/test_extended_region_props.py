6e20e3e462f1d3ced1a3ad6c60a3250908f40060
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-05 15:24:16 -0400

    test_extended_region_props: Added test for ExtendedRegionProps construction and immutable methods from advanced_image_processing. This finally fixes #110.

--- tests/test_extended_region_props.py
+++ tests/test_extended_region_props.py
@@ -198,3 +198,58 @@ class TestAdvancedImageProcessing(object):
 
         assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_label_image().nonzero(), tuple(p.T))]))
 
+    def test_ExtendedRegionProps_5(self):
+        p = numpy.array([[27, 51],
+                         [66, 85],
+                         [77, 45]])
+
+        space = numpy.array((100, 100))
+        radii = numpy.array((5, 6, 7))
+        magnitudes = numpy.array((1, 1, 1), dtype = float)
+
+        g = synthetic_data.generate_gaussian_images(space, p, radii/3.0, magnitudes/3)
+        m = (g > 0.00065)
+        g *= m
+
+        e = nanshe.advanced_image_processing.ExtendedRegionProps(g.max(axis = 0),
+                nanshe.expanded_numpy.enumerate_masks(m).max(axis = 0)
+        )
+
+        removal_mask = numpy.zeros(radii.shape, dtype = bool)
+        removal_mask[0] = True
+
+        m = m[~removal_mask].copy()
+        p = p[~removal_mask].copy()
+        radii = radii[~removal_mask].copy()
+
+        g = g * m.max(axis = 0)
+
+        e.remove_prop_indices(*removal_mask.nonzero()[0])
+
+        print len(e.props)
+        print len(p)
+
+
+        assert(len(e.props) == len(p))
+
+        assert((e.count["label"] == numpy.arange(1, len(m) + 1)).all())
+
+        assert((e.count["count"] == 1).all())
+
+        assert((e.label_image == nanshe.expanded_numpy.enumerate_masks(m).max(axis = 0)).all())
+
+        assert((e.intensity_image == g.max(axis = 0)).all())
+
+        assert((e.image_mask == m.max(axis = 0)).all())
+
+        assert((e.props == nanshe.advanced_image_processing.extended_region_local_maxima_properties(g.max(axis = 0),
+            nanshe.expanded_numpy.enumerate_masks(m).max(axis = 0), properties = ["label", "centroid"])).all()
+        )
+
+        assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_index_array(), tuple(p.T))]))
+
+        assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_label_image().nonzero(), tuple(p.T))]))
+
+        assert((e.get_local_max_label_image()[e.get_local_max_label_image().nonzero()] == numpy.arange(1, len(m) + 1)).all())
+
+        assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_label_image().nonzero(), tuple(p.T))]))
