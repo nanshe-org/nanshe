ff057a146c8f1acc919a514f3a6548a426e8aa69
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-05 15:21:55 -0400

    test_extended_region_props: Added another test for ExtendedRegionProps construction and immutable methods from advanced_image_processing. This partially addresses #110.

--- tests/test_extended_region_props.py
+++ tests/test_extended_region_props.py
@@ -55,3 +55,49 @@ class TestAdvancedImageProcessing(object):
 
         assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_label_image().nonzero(), tuple(p.T))]))
 
+    def test_ExtendedRegionProps_2(self):
+        p = numpy.array([[27, 51],
+                         [32, 53],
+                         [77, 45]])
+
+        space = numpy.array((100, 100))
+        radii = numpy.array((5, 6, 7))
+        magnitudes = numpy.array((1, 1, 1), dtype = float)
+
+        g = synthetic_data.generate_gaussian_images(space, p, radii/3.0, magnitudes/3)
+        g = numpy.array([g[0] + g[1], g[2]])
+        m = (g > 0.00065)
+        g *= m
+
+        e = nanshe.advanced_image_processing.ExtendedRegionProps(g.max(axis = 0),
+                nanshe.expanded_numpy.enumerate_masks(m).max(axis = 0)
+        )
+
+        removal_mask = numpy.zeros(radii.shape, dtype = bool)
+
+        e.remove_prop_mask(removal_mask)
+
+        assert(len(e.props) == len(p))
+
+        assert((e.count["label"] == numpy.arange(1, len(m) + 1)).all())
+
+        assert((e.count["count"] == numpy.array([2, 1])).all())
+
+        assert((e.label_image == nanshe.expanded_numpy.enumerate_masks(m).max(axis = 0)).all())
+
+        assert((e.intensity_image == g.max(axis = 0)).all())
+
+        assert((e.image_mask == m.max(axis = 0)).all())
+
+        assert((e.props == nanshe.advanced_image_processing.extended_region_local_maxima_properties(g.max(axis = 0),
+            nanshe.expanded_numpy.enumerate_masks(m).max(axis = 0), properties = ["label", "centroid"])).all()
+        )
+
+        assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_index_array(), tuple(p.T))]))
+
+        assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_label_image().nonzero(), tuple(p.T))]))
+
+        assert((e.get_local_max_label_image()[e.get_local_max_label_image().nonzero()] == numpy.array([1, 1, 2])).all())
+
+        assert(all([(_1 == _2).all() for _1, _2 in itertools.izip(e.get_local_max_label_image().nonzero(), tuple(p.T))]))
+
