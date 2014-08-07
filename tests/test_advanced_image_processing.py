2be5eb6888a8f104f3c51795e00e5fb6fb56a4bc
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-07 10:13:28 -0400

    test_advanced_image_processing: Added a third test for remove_too_close_local_maxima from advanced_image_processing. This partially addresses #112.

--- tests/test_advanced_image_processing.py
+++ tests/test_advanced_image_processing.py
@@ -330,6 +330,33 @@ class TestAdvancedImageProcessing(object):
 
         assert(len(points) == len(e2.props))
 
+    def test_remove_too_close_local_maxima_3(self):
+        space = numpy.array((100, 100))
+        radii = numpy.array((5, 5))
+        magnitudes = numpy.array((1, 1.01), dtype = float)
+        points = numpy.array([[63, 69],
+                              [58, 64]])
+
+        masks = synthetic_data.generate_hypersphere_masks(space, points, radii)
+        images = synthetic_data.generate_gaussian_images(space, points, radii/3.0, magnitudes) * masks
+        labels = masks.max(axis = 0).astype(int)
+
+        e = nanshe.advanced_image_processing.ExtendedRegionProps(images.max(axis = 0), labels)
+
+        dist = scipy.spatial.distance.pdist(points).max()
+        i = 0
+        while (dist + i * numpy.finfo(type(dist)).eps) == dist:
+            i += 1
+        dist += i * numpy.finfo(type(dist)).eps
+
+        e2 = nanshe.advanced_image_processing.remove_too_close_local_maxima(e, dist)
+
+        assert(len(points) == len(e.props))
+
+        assert(1 == len(e2.props))
+
+        assert((points[magnitudes == magnitudes.max()] == e2.props["local_max"][0]).all())
+
     def test_wavelet_denoising(self):
         params = {
             "remove_low_intensity_local_maxima" : {
