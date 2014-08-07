8edc4277869d977faa294e2e92cbf170bf0984e9
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-07 17:05:07 -0400

    test_advanced_image_processing: Added a test for preprocess_data from advanced_image_processing. This partially addresses #101.

--- tests/test_advanced_image_processing.py
+++ tests/test_advanced_image_processing.py
@@ -121,6 +121,54 @@ class TestAdvancedImageProcessing(object):
         # Turns out that a difference greater than 0.1 will be over 10 standard deviations away.
         assert( ((a - 100.0*b) < 0.1).all() )
 
+    def test_preprocess_data(self):
+        ## Does NOT test accuracy.
+
+        config = {
+            "normalize_data" : {
+                "simple_image_processing.renormalized_images" : {
+                    "ord" : 2
+                }
+            },
+            "extract_f0" : {
+                "spatial_smoothing_gaussian_filter_stdev" : 5.0,
+                "which_quantile" : 0.5,
+                "temporal_smoothing_gaussian_filter_stdev" : 5.0,
+                "half_window_size" : 20,
+                "bias" : 100,
+                "step_size" : 10
+            },
+            "remove_zeroed_lines" : {
+                "erosion_shape" : [
+                    21,
+                    1
+                ],
+                "dilation_shape" : [
+                    1,
+                    3
+                ]
+            },
+            "wavelet_transform" : {
+                "scale" : [
+                    3,
+                    4,
+                    4
+                ]
+            }
+        }
+
+        space = numpy.array([100, 100, 100])
+        radii = numpy.array([5, 6])
+        magnitudes = numpy.array([15, 16])
+        points = numpy.array([[20, 30, 24],
+                           [70, 59, 65]])
+
+        masks = synthetic_data.generate_hypersphere_masks(space, points, radii)
+        images = synthetic_data.generate_gaussian_images(space, points, radii/3.0, magnitudes) * masks
+        image_stack = images.max(axis = 0)
+
+        nanshe.advanced_image_processing.preprocess_data(image_stack, **config)
+
     def test_generate_dictionary(self):
         p = numpy.array([[27, 51],
                      [66, 85],
