35e6613b89c03cb53d27572f20f3a5edd1202a95
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-15 12:43:24 -0400

    tests/test_advanced_image_processing: Added a test for generate_local_maxima using 3D data.

--- tests/test_advanced_image_processing.py
+++ tests/test_advanced_image_processing.py
@@ -595,12 +595,26 @@ class TestAdvancedImageProcessing(object):
 
         assert((numpy.array(m.nonzero()) == p.T).all())
 
-    def test_generate_local_maxima(self):
+    def test_generate_local_maxima_1(self):
         p = numpy.array([[27, 51],
                          [66, 85],
                          [77, 45]])
 
         space = numpy.array((100, 100))
+        radii = numpy.array((5, 6, 7))
+        magnitudes = numpy.array((1, 1, 1), dtype = float)
+
+        g = synthetic_data.generate_gaussian_images(space, p, radii/3.0, magnitudes/3)
+        m = nanshe.advanced_image_processing.generate_local_maxima(g.max(axis = 0))
+
+        assert((numpy.array(m.nonzero()) == p.T).all())
+
+    def test_generate_local_maxima_2(self):
+        p = numpy.array([[27, 51, 87],
+                         [66, 85, 55],
+                         [77, 45, 26]])
+
+        space = numpy.array((100, 100, 100))
         radii = numpy.array((5, 6, 7))
         magnitudes = numpy.array((1, 1, 1), dtype = float)
 
