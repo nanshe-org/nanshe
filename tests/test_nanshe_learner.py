3b8b682c3fba772933ca491681ed68cbf9a89e81
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-08-26 00:06:57 -0400

    tests/test_nanshe_learner.py: Added a test for nanshe.nanshe_learner.generate_neurons_blocks with blocks using 3D data.

--- tests/test_nanshe_learner.py
+++ tests/test_nanshe_learner.py
@@ -1167,6 +1167,35 @@ class TestNansheLearner(object):
 
         assert(len(unmatched_points) == 0)
 
+    def test_generate_neurons_blocks_3(self):
+        nanshe.nanshe_learner.generate_neurons_blocks(self.hdf5_input_3D_filepath, self.hdf5_output_3D_filepath, **self.config_blocks_3D["generate_neurons_blocks"])
+
+        assert(os.path.exists(self.hdf5_output_3D_filename))
+
+        with h5py.File(self.hdf5_output_3D_filename, "r") as fid:
+            assert("neurons" in fid)
+
+            neurons = fid["neurons"].value
+
+        assert(len(self.points3) == len(neurons))
+
+        neuron_maxes = (neurons["image"] == nanshe.expanded_numpy.expand_view(neurons["max_F"], neurons["image"].shape[1:]))
+        neuron_max_points = numpy.array(neuron_maxes.max(axis = 0).nonzero()).T.copy()
+
+        matched = dict()
+        unmatched_points = numpy.arange(len(self.points3))
+        for i in xrange(len(neuron_max_points)):
+            new_unmatched_points = []
+            for j in unmatched_points:
+                if not (neuron_max_points[i] == self.points3[j]).all():
+                    new_unmatched_points.append(j)
+                else:
+                    matched[i] = j
+
+            unmatched_points = new_unmatched_points
+
+        assert(len(unmatched_points) == 0)
+
     def test_generate_neurons(self):
         with h5py.File(self.hdf5_output_filename, "a") as output_file_handle:
             output_group = output_file_handle["/"]
