9aa6aacc368ef94bdabfa9d02f254573788e8b39
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-09-04 11:59:54 -0400

    nanshe/advanced_image_processing.py: Fixed spelling error in comment.

--- nanshe/advanced_image_processing.py
+++ nanshe/advanced_image_processing.py
@@ -1759,7 +1759,7 @@ def wavelet_denoising(new_image,
             else:
                 is_upper_bounded_maybe = numpy.ones(new_wavelet_image_denoised_labeled_props.shape, dtype = bool)
 
-            # See whether both or neither bound is satisified.
+            # See whether both or neither bound is satisfied.
             is_within_bound = is_lower_bounded_maybe & is_upper_bounded_maybe
             is_not_within_bound = ~is_within_bound
 
