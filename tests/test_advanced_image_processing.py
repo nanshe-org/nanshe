5a6ba10f5bff5e4688a34ca7657d55e2c9d02f5a
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-08 13:47:08 -0400

    tests/test_advanced_image_processing.py: Added a test for remove_zeroed_lines, which exploits linear interpolation.

--- tests/test_advanced_image_processing.py
+++ tests/test_advanced_image_processing.py
@@ -124,6 +124,26 @@ class TestAdvancedImageProcessing(object):
 
         assert(numpy.allclose(a, b, rtol=0, atol=1e-13))
 
+    def test_remove_zeroed_lines_7(self):
+        a = numpy.repeat(numpy.arange(100)[None], 101, axis = 0)[None].astype(float)
+        a[0, :, 0] = 1
+        nanshe.expanded_numpy.index_axis_at_pos(nanshe.expanded_numpy.index_axis_at_pos(a, 0, 0), -1, 0)[:] = 1
+
+        erosion_shape = [ 21, 1 ]
+        dilation_shape = [ 1, 3 ]
+
+        r = numpy.array([[0, 0, 0, 0], [0, 2, 3, 4]]).T.copy()
+
+        print(r)
+
+        ar = a.copy()
+        for each_r in r:
+            nanshe.expanded_numpy.index_axis_at_pos(nanshe.expanded_numpy.index_axis_at_pos(ar, 0, each_r[0]), -1, each_r[-1])[:] = 0
+
+        b = nanshe.advanced_image_processing.remove_zeroed_lines(ar, erosion_shape=erosion_shape, dilation_shape=dilation_shape)
+
+        assert(numpy.allclose(a, b, rtol=0, atol=1e-13))
+
     def test_extract_f0_1(self):
         spatial_smoothing_gaussian_filter_stdev = 5.0
         which_quantile = 0.5
