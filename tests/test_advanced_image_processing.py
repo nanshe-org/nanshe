5895b3ce27745862d9a589fecf6fb69033495315
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-09 20:28:10 -0400

    tests/test_advanced_image_processing.py: Added test_remove_zeroed_lines_9 to test in-place calculations.

--- tests/test_advanced_image_processing.py
+++ tests/test_advanced_image_processing.py
@@ -162,6 +162,24 @@ class TestAdvancedImageProcessing(object):
 
         assert((a == b).all())
 
+    def test_remove_zeroed_lines_9(self):
+        a = numpy.ones((1, 100, 101))
+        erosion_shape = [ 21, 1 ]
+        dilation_shape = [ 1, 3 ]
+
+        r = numpy.array([[0, 0, 0], [a.shape[1]-2, 3, 4]]).T.copy()
+
+        print(r)
+
+        ar = a.copy()
+        for each_r in r:
+            nanshe.expanded_numpy.index_axis_at_pos(nanshe.expanded_numpy.index_axis_at_pos(ar, 0, each_r[0]), -1, each_r[-1])[:] = 0
+
+        b = ar
+        nanshe.advanced_image_processing.remove_zeroed_lines(b, erosion_shape=erosion_shape, dilation_shape=dilation_shape, out=b)
+
+        assert((a == b).all())
+
     def test_extract_f0_1(self):
         spatial_smoothing_gaussian_filter_stdev = 5.0
         which_quantile = 0.5
