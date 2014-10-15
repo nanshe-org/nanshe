b69e427d7c1f4cf3ed2dccc70ec2e89e084dfa15
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-15 18:28:21 -0400

    synthetic_data/__init__.py: Created new module for synthetic data generation.

---
+++ synthetic_data/__init__.py
@@ -0,0 +1,11 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Oct 15, 2014 18:16:33 EDT$"
+
+__all__ = [".."]
+
+
+
