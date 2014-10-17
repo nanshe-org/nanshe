d67ca80e5222b0a1fcdfe4643396e547792ff95a
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-16 23:23:56 -0400

    nanshe/__init__.py: Added binary_image_processing to the module imports.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -5,13 +5,14 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 8:37:08PM$"
 
-__all__ = ["..", "debugging_tools", "advanced_image_processing", "additional_generators", "expanded_numpy",
-           "nanshe_learner", "denoising", "HDF5_recorder", "HDF5_searchers", "HDF5_serializers", #"nanshe_viewer",
+__all__ = ["..", "debugging_tools", "advanced_image_processing", "additional_generators", "binary_image_processing",
+           "expanded_numpy", "nanshe_learner", "denoising", "HDF5_recorder", "HDF5_searchers", "HDF5_serializers", #"nanshe_viewer",
            "read_config", "simple_image_processing", "wavelet_transform"]
 
 import debugging_tools
 import advanced_image_processing
 import additional_generators
+import binary_image_processing
 import expanded_numpy
 import nanshe_learner
 import denoising
