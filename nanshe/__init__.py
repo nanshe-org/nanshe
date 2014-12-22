61606ebd255fc1f53c62d6f81894484856ed574e
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-22 09:01:15 -0500

    spams_sandbox -> nanshe/spams_sandbox: Moved spams_sandbox into nanshe.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -1,6 +1,7 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 22, 2014 08:46:12 EST$"
 
-__all__ = ["nanshe"]
+__all__ = ["nanshe", "spams_sandbox"]
 
 import nanshe
+import spams_sandbox
