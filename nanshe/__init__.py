a9c52016303dea64f99b95faef342706c60bde4b
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-22 09:04:24 -0500

    synthetic_data -> nanshe/synthetic_data: Moved the module synthetic_data into nanshe.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -1,7 +1,8 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 22, 2014 08:46:12 EST$"
 
-__all__ = ["nanshe", "spams_sandbox"]
+__all__ = ["nanshe", "spams_sandbox", "synthetic_data"]
 
 import nanshe
 import spams_sandbox
+import synthetic_data
