82f473d9e5cf8527f12670b8c6ca9ddebc10593a
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-05 08:17:45 -0500

    nanshe/nanshe/wavelet_transform.py: Replace ".shape[0]" with len.

--- nanshe/nanshe/wavelet_transform.py
+++ nanshe/nanshe/wavelet_transform.py
@@ -295,10 +295,10 @@ def wavelet_transform(im0, scale = 5, include_intermediates = False, include_low
             raise Exception(
                 "Scale should only have 1 dimension. Instead, got scale.ndim = \"" + str(scale.ndim) + "\".")
 
-        if scale.shape[0] != im0.ndim:
+        if len(scale) != im0.ndim:
             raise Exception(
-                "Scale should have a value of each dimension of im0. Instead, got scale.shape[0] = \"" + str(
-                    scale.shape[0]) + "\" and im0.ndim = \"" + str(im0.ndim) + "\".")
+                    "Scale should have a value of each dimension of im0. Instead, got len(scale) = \"" + str(
+                        len(scale)) + "\" and im0.ndim = \"" + str(im0.ndim) + "\".")
 
     except TypeError:
         scale = numpy.repeat([scale], im0.ndim)
