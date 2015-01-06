0d0aa220c4b5666e330bbbc7aab392c80f8a91e5
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-06 15:58:19 -0500

    docs/make.bat: Exclude test directory from being documented.

--- docs/make.bat
+++ docs/make.bat
@@ -83,7 +83,7 @@ if errorlevel 9009 (
 )
 
 if "%1" == "rst" (
-        %SPHINXAPIDOC% -f -o . ..
+        %SPHINXAPIDOC% -f -o . .. ../tests
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished. The RST pages are in in the current directory.
