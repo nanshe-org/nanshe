36597d4289508280612d3b46f66a5f8579611224
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-27 16:46:51 -0500

    nanshe/nanshe/expanded_numpy.py: Updated min_abs to use new behavior of squish.

--- nanshe/nanshe/expanded_numpy.py
+++ nanshe/nanshe/expanded_numpy.py
@@ -688,10 +688,6 @@ def min_abs(new_array, axis=None, keepdims=False):
     # Slice out relevant results
     result = new_array_refolded[result_indices]
 
-    # Add back last dimension as needed.
-    if keepdims:
-        result = result[..., None]
-
     return(result)
 
 
