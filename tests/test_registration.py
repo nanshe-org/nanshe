b32ca3f81d70de9be2887ce33b810e0fd0b5d410
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-20 11:10:02 -0500

    tests/test_registration.py: Provide two tests for register_mean_offsets.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,3 +6,45 @@ __date__ = "$Feb 20, 2015 10:40:15 EST$"
 import numpy
 
 import nanshe.nanshe.registration
+
+
+class TestRegisterMeanOffsets(object):
+    def test1(self):
+        a = numpy.zeros((20,10,11), dtype=int)
+
+        a[:, 3:-3, 3:-3] = 1
+
+        b = numpy.ma.masked_array(a.copy())
+
+        a[10] = 0
+        a[10, :-6, :-6] = 1
+
+        b[10, :, :3] = numpy.ma.masked
+        b[10, :3, :] = numpy.ma.masked
+
+
+        b2 = nanshe.nanshe.registration.register_mean_offsets(a)
+
+        assert (b2.dtype == b.dtype)
+        assert (b2.data == b.data).all()
+        assert (b2.mask == b.mask).all()
+
+    def test2(self):
+        a = numpy.zeros((20,11,12), dtype=int)
+
+        a[:, 3:-4, 3:-4] = 1
+
+        b = numpy.ma.masked_array(a.copy())
+
+        a[10] = 0
+        a[10, :-7, :-7] = 1
+
+        b[10, :, :3] = numpy.ma.masked
+        b[10, :3, :] = numpy.ma.masked
+
+
+        b2 = nanshe.nanshe.registration.register_mean_offsets(a)
+
+        assert (b2.dtype == b.dtype)
+        assert (b2.data == b.data).all()
+        assert (b2.mask == b.mask).all()
