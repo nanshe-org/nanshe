1a51735c8a3ad8aa9281f1fe6234f1fb32612470
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-20 10:42:35 -0500

    tests/__init__.py: Import test_registration.

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@
 __author__="John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ ="$Jun 6, 2014 4:21:22PM$"
 
-__all__ = ["..", "test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_extended_region_props", "test_HDF5_searchers", "test_HDF5_serializers", "test_nanshe_learner", "test_spams_sandbox", "test_tiff_file_format", "random_dictionary_learning_data"]
+__all__ = ["..", "test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_extended_region_props", "test_HDF5_searchers", "test_HDF5_serializers", "test_nanshe_learner", "test_registration", "test_spams_sandbox", "test_tiff_file_format", "random_dictionary_learning_data"]
 
 
 import test_additional_io
@@ -15,6 +15,7 @@ import test_extended_region_props
 import test_HDF5_searchers
 import test_HDF5_serializers
 import test_nanshe_learner
+import test_registration
 import test_spams_sandbox
 import test_tiff_file_format
 import random_dictionary_learning_data
