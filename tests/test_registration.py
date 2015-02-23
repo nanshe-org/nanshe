e5d0f64c7862ad2851032c27a36a8b6c26784551
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-23 16:15:36 -0500

    tests/test_registration.py: Added tests for `find_offsets`.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -48,3 +48,49 @@ class TestRegisterOffsets(object):
         assert (b2.dtype == b.dtype)
         assert (b2.data == b.data).all()
         assert (b2.mask == b.mask).all()
+
+
+class TestFindOffsets(object):
+    def test1(self):
+        a = numpy.zeros((20,10,11), dtype=int)
+        a_off = numpy.zeros((len(a), a.ndim-1), dtype=int)
+
+        a[:, 3:-3, 3:-3] = 1
+
+        a[10] = 0
+        a[10, :-6, :-6] = 1
+
+        a_off[10] = 3
+
+        am = a.mean(axis=0)
+
+        af = numpy.fft.fftn(a, axes=range(1, a.ndim))
+        amf = numpy.fft.fftn(am, axes=range(am.ndim))
+
+
+        a_off2 = nanshe.nanshe.registration.find_offsets(af, amf)
+
+        assert (a_off2.dtype == a_off.dtype)
+        assert (a_off2 == a_off).all()
+
+    def test2(self):
+        a = numpy.zeros((20,11,12), dtype=int)
+        a_off = numpy.zeros((len(a), a.ndim-1), dtype=int)
+
+        a[:, 3:-4, 3:-4] = 1
+
+        a[10] = 0
+        a[10, :-7, :-7] = 1
+
+        a_off[10] = 3
+
+        am = a.mean(axis=0)
+
+        af = numpy.fft.fftn(a, axes=range(1, a.ndim))
+        amf = numpy.fft.fftn(am, axes=range(am.ndim))
+
+
+        a_off2 = nanshe.nanshe.registration.find_offsets(af, amf)
+
+        assert (a_off2.dtype == a_off.dtype)
+        assert (a_off2 == a_off).all()
