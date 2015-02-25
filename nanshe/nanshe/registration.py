9b8e90c2d62f9dcd9c0b68907e2d13982db9883e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-25 15:32:14 -0500

    nanshe/nanshe/registration.py: Switch from numpy.dot to numpy.tensordot.

--- nanshe/nanshe/registration.py
+++ nanshe/nanshe/registration.py
@@ -135,7 +135,7 @@ def register_mean_offsets(frames2reg, max_iters=-1, include_shift=False):
     while (squared_magnitude_delta_space_shift != 0.0):
         squared_magnitude_delta_space_shift = 0.0
 
-        frames2reg_shifted_fft[...] = frames2reg_fft * numpy.exp(1j * numpy.dot(space_shift, unit_space_shift_fft.T)).reshape(frames2reg_fft.shape)
+        frames2reg_shifted_fft[...] = frames2reg_fft * numpy.exp(1j * numpy.tensordot(space_shift, unit_space_shift_fft, axes=[-1, -1])).reshape(frames2reg_fft.shape)
         template_fft[:] = numpy.mean(frames2reg_shifted_fft, axis=0)
 
         this_space_shift = find_offsets(frames2reg_fft, template_fft)
