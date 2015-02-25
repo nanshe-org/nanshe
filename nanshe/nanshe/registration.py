69dfa8aa5147e5eb374485866ec88a51fef43cd6
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-25 00:42:47 -0500

    nanshe/nanshe/registration.py: Store the intermediate results of the frame translations and compute the mean as a final step.

--- nanshe/nanshe/registration.py
+++ nanshe/nanshe/registration.py
@@ -118,6 +118,7 @@ def register_mean_offsets(frames2reg, max_iters=-1, include_shift=False):
     reg_frames.mask = numpy.ma.getmaskarray(reg_frames)
 
     frames2reg_fft = numpy.fft.fftn(frames2reg, axes=range(1, frames2reg.ndim))
+    frames2reg_shifted_fft = numpy.empty_like(frames2reg_fft)
     template_fft = numpy.empty(frames2reg.shape[1:], dtype=complex)
 
     # Repeat shift calculation until there is no further adjustment.
@@ -126,11 +127,9 @@ def register_mean_offsets(frames2reg, max_iters=-1, include_shift=False):
     while (squared_magnitude_delta_space_shift != 0.0):
         squared_magnitude_delta_space_shift = 0.0
 
-        template_fft[:] = 0
-        for i in xrange(len(frames2reg_fft)):
-            for template_fft_index in itertools.product(*[xrange(_) for _ in template_fft.shape]):
-                template_fft[template_fft_index] += frames2reg_fft[(i,) + template_fft_index] * numpy.exp(1j * -2*numpy.pi * (space_shift[i] * numpy.asarray(template_fft_index) / numpy.asarray(template_fft.shape, dtype=float)).sum())
-        template_fft /= len(frames2reg_fft)
+        for frames2reg_fft_index in itertools.product(*[xrange(_) for _ in frames2reg_fft.shape]):
+            frames2reg_shifted_fft[frames2reg_fft_index] = frames2reg_fft[frames2reg_fft_index] * numpy.exp(1j * -2*numpy.pi * (space_shift[frames2reg_fft_index[0]] * numpy.asarray(frames2reg_fft_index[1:]) / numpy.asarray(template_fft.shape, dtype=float)).sum())
+        template_fft[:] = numpy.mean(frames2reg_shifted_fft, axis=0)
 
         this_space_shift = find_offsets(frames2reg_fft, template_fft)
 
