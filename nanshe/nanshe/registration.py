b03c591c7fe2a7a645568de965c45b90f324e067
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-25 13:14:48 -0500

    nanshe/nanshe/registration.py: Compute the negative of the wave vector in advance.

--- nanshe/nanshe/registration.py
+++ nanshe/nanshe/registration.py
@@ -121,9 +121,10 @@ def register_mean_offsets(frames2reg, max_iters=-1, include_shift=False):
     frames2reg_shifted_fft = numpy.empty_like(frames2reg_fft)
     template_fft = numpy.empty(frames2reg.shape[1:], dtype=complex)
 
-    wave_vector = numpy.asarray(template_fft.shape, dtype=float)
-    numpy.reciprocal(wave_vector, out=wave_vector)
-    wave_vector *= 2*numpy.pi
+    negative_wave_vector = numpy.asarray(template_fft.shape, dtype=float)
+    numpy.reciprocal(negative_wave_vector, out=negative_wave_vector)
+    negative_wave_vector *= 2*numpy.pi
+    numpy.negative(negative_wave_vector, out=negative_wave_vector)
 
     # Repeat shift calculation until there is no further adjustment.
     num_iters = 0
@@ -132,7 +133,7 @@ def register_mean_offsets(frames2reg, max_iters=-1, include_shift=False):
         squared_magnitude_delta_space_shift = 0.0
 
         for frames2reg_fft_index in itertools.product(*[xrange(_) for _ in frames2reg_fft.shape]):
-            frames2reg_shifted_fft[frames2reg_fft_index] = frames2reg_fft[frames2reg_fft_index] * numpy.exp(-1j * numpy.dot(space_shift[frames2reg_fft_index[0]], numpy.asarray(frames2reg_fft_index[1:]) * wave_vector))
+            frames2reg_shifted_fft[frames2reg_fft_index] = frames2reg_fft[frames2reg_fft_index] * numpy.exp(1j * numpy.dot(space_shift[frames2reg_fft_index[0]], numpy.asarray(frames2reg_fft_index[1:]) * negative_wave_vector))
         template_fft[:] = numpy.mean(frames2reg_shifted_fft, axis=0)
 
         this_space_shift = find_offsets(frames2reg_fft, template_fft)
