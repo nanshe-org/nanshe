6d1b1e59fb5347b5cb77dfe132cd215d7d4035f3
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-24 07:50:31 -0400

    tests/test_HDF5_recorder.py: Add some basic tests for `EmptyArrayRecorder`.

--- tests/test_HDF5_recorder.py
+++ tests/test_HDF5_recorder.py
@@ -3,3 +3,78 @@ __date__ = "$Mar 24, 2015 07:35:20 EDT$"
 
 
 
+import numpy
+
+import nanshe.nanshe.HDF5_recorder
+
+
+class TestHDF5Recorder(object):
+    def test_EmptyArrayRecorder(self):
+        recorder = nanshe.nanshe.HDF5_recorder.EmptyArrayRecorder()
+
+
+        # Check if this stores results.
+
+        assert not recorder
+
+
+        # Check for missing key.
+
+        assert recorder.get("key") is None
+
+        assert recorder.get("key", True)
+
+        assert "key" not in recorder
+
+        got_key_error = False
+        try:
+            recorder["key"]
+        except KeyError:
+            got_key_error = True
+
+        assert got_key_error
+
+
+        # Add subgroup key and check for it.
+
+        recorder["key"] = None
+
+        assert recorder.get("key") is not None
+
+        assert "key" in recorder
+
+        got_key_error = False
+        try:
+            recorder["key"]
+        except KeyError:
+            got_key_error = True
+
+        assert not got_key_error
+
+
+        # Add data
+
+        got_value_error = False
+        try:
+            recorder["value"] = numpy.array([])
+        except ValueError:
+            got_value_error = True
+        assert got_value_error
+
+        recorder["value"] = numpy.array(0)
+
+        got_key_error = False
+        try:
+            recorder["value"]
+        except KeyError:
+            got_key_error = True
+        assert got_key_error
+
+        recorder["key"]["value"] = numpy.array(0)
+
+        got_key_error = False
+        try:
+            recorder["key"]["value"]
+        except KeyError:
+            got_key_error = True
+        assert got_key_error
