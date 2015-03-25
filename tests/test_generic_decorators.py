541d0aa1fe2d7db7b27c8e028bae0cc6b263fac4
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-25 13:40:38 -0400

    tests/test_generic_decorators.py: Add test for `update_wrapper` from `generic_decorators`.

--- tests/test_generic_decorators.py
+++ tests/test_generic_decorators.py
@@ -2,4 +2,29 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 25, 2015 13:30:52 EDT$"
 
 
+import functools
+
 import nanshe.nanshe.generic_decorators
+
+
+
+class TestGenericDecorators(object):
+    def test_update_wrapper(self):
+        def wrapper(a_callable):
+            def wrapped(*args, **kwargs):
+                return(a_callable(*args, **kwargs))
+
+            return(wrapped)
+
+        def func(a, b=2):
+            return(a + b)
+
+        func_wrapped_1 = functools.update_wrapper(wrapper, func)
+        if not hasattr(func_wrapped_1, "__wrapped__"):
+            setattr(func_wrapped_1, "__wrapped__", func)
+
+        func_wrapped_2 = nanshe.nanshe.generic_decorators.update_wrapper(
+            wrapper, func
+        )
+
+        assert func_wrapped_1 == func_wrapped_2
