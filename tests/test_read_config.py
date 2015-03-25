ff9bea9da9b974f55ac13d606fb5b1f94fbc25e7
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-24 22:58:55 -0400

    tests/test_read_config.py: Add test using ordered dictionary.

--- tests/test_read_config.py
+++ tests/test_read_config.py
@@ -3,6 +3,7 @@ __date__ = "$Mar 24, 2015 22:04:14 EDT$"
 
 
 
+import collections
 import json
 import os
 import shutil
@@ -37,6 +38,32 @@ class TestReadConfig(object):
 
         params_out = nanshe.nanshe.read_config.read_parameters(
             config_filename
+        )
+
+        assert params == params_out
+
+
+    def test1(self):
+        dict_type = collections.OrderedDict
+
+        params = dict_type()
+        params["b"] = range(3)
+        params["c"] = "test"
+        params["a"] = 5
+        params["d"] = dict_type(params)
+        params["h"] = [dict_type(params["d"])]
+        params["g"] = [[_k, _v] for _k, _v in params["d"].items()]
+
+        config_filename = os.path.join(self.temp_dir, "config.json")
+        with open(config_filename, "w") as config_file:
+            json.dump(params, config_file)
+
+        with open(config_filename, "r") as config_file:
+            params_raw_out = json.load(config_file)
+            assert params == params_raw_out
+
+        params_out = nanshe.nanshe.read_config.read_parameters(
+            config_filename, maintain_order=True
         )
 
         assert params == params_out
