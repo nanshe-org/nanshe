ff766e75f52d5a0f3236269a70759d98f6e9f465
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-24 22:56:47 -0400

    tests/test_read_config.py: Setup and remove temporary directory for testing.

--- tests/test_read_config.py
+++ tests/test_read_config.py
@@ -3,4 +3,18 @@ __date__ = "$Mar 24, 2015 22:04:14 EDT$"
 
 
 
+import shutil
+import tempfile
+
 import nanshe.nanshe.read_config
+
+
+class TestReadConfig(object):
+    def setup(self):
+        self.temp_dir = tempfile.mkdtemp()
+
+
+    def teardown(self):
+        shutil.rmtree(self.temp_dir)
+
+        self.temp_dir = ""
