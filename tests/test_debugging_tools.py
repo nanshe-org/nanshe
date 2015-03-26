5bb66e34fa41cdf0ac6e0d329007d2375c8b904e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-26 17:00:49 -0400

    tests/test_debugging_tools.py: Space out `__author__` and `__date__`.

--- tests/test_debugging_tools.py
+++ tests/test_debugging_tools.py
@@ -1,5 +1,5 @@
-__author__="John Kirkham <kirkhamj@janelia.hhmi.org>"
-__date__ ="$July 30, 2014 4:57:43PM$"
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$July 30, 2014 4:57:43PM$"
 
 
 import StringIO
