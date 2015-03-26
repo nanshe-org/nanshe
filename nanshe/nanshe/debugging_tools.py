4b71b6824a198eb68125b969dd3a99aa48eb672c
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-26 16:07:26 -0400

    nanshe/nanshe/debugging_tools.py: Remove old default header.

--- nanshe/nanshe/debugging_tools.py
+++ nanshe/nanshe/debugging_tools.py
@@ -1,8 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 5:42:07PM$"
 
