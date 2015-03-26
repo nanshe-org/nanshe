04bd70fa88b68205b84bc2ed88ca0d1b9a851115
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-26 16:07:56 -0400

    nanshe/nanshe/denoising.py: Remove old default header.

--- nanshe/nanshe/denoising.py
+++ nanshe/nanshe/denoising.py
@@ -1,7 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$May 1, 2014 2:23:45PM$"
 
