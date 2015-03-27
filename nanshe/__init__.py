00c735c5046596c18bff3e5f09e95113efd9b08d
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-27 09:33:48 -0400

    nanshe/hdf5/__init__.py: Add the module `hdf5` to contain a bunch of utility functions and classes to enable simpler use of HDF5. This is built on h5py. nanshe/__init__.py: Add the `hdf5` module to the list of imports.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -2,8 +2,9 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 22, 2014 08:46:12 EST$"
 
 
-__all__ = ["nanshe", "spams_sandbox", "synthetic_data", "util"]
+__all__ = ["hdf5", "nanshe", "spams_sandbox", "synthetic_data", "util"]
 
+import hdf5
 import nanshe
 import spams_sandbox
 import synthetic_data
