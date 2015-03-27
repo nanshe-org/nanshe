a83db2d261d3b8c826ce9356506b4b94f9f70fa4
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-27 09:30:56 -0400

    nanshe/util/__init__.py: Add new module `util` for containing a bunch of basic utility modules and functions. nanshe/__init__.py: Add the `util` module to the list of imports.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -2,8 +2,9 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 22, 2014 08:46:12 EST$"
 
 
-__all__ = ["nanshe", "spams_sandbox", "synthetic_data"]
+__all__ = ["nanshe", "spams_sandbox", "synthetic_data", "util"]
 
 import nanshe
 import spams_sandbox
 import synthetic_data
+import util
---
+++ nanshe/util/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Mar 27, 2015 09:28:53 EDT$"
+
+
