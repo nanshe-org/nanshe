323c1b40406a1505de0db488a0011a221f4bdcce
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-27 20:54:55 -0400

    tests/test_hdf5/__init__.py: Add new test module `test_hdf5` for containing tests related to the module `hdf5`. tests/__init__.py: Add `test_hdf5` to the list of imports.

--- tests/test_nanshe/__init__.py
+++ tests/test_nanshe/__init__.py
@@ -1,9 +1,10 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 27, 2015 19:27:12 EDT$"
 
-__all__ = ["test_nanshe", "test_spams_sandbox", "test_util"]
+__all__ = ["test_hdf5", "test_nanshe", "test_spams_sandbox", "test_util"]
 
 
+import test_hdf5
 import test_nanshe
 import test_spams_sandbox
 import test_util
---
+++ tests/test_nanshe/test_hdf5/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Mar 27, 2015 19:26:55 EDT$"
+
+__all__ = []
+
+
+
