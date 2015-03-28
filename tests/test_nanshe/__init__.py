b3b4528ce9cb641acef6911b35877ef3da7acfb9
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-27 20:34:30 -0400

    tests/: Cleanup test directory to match the module layout.

---
+++ tests/test_nanshe/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Mar 27, 2015 19:27:12 EDT$"
+
+__all__ = ["test_nanshe", "test_spams_sandbox"]
+
+
+import test_nanshe
+import test_spams_sandbox
---
+++ tests/test_nanshe/test_spams_sandbox/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Mar 27, 2015 19:27:42 EDT$"
+
+__all__ = ["test_spams_sandbox"]
+
+
+import test_spams_sandbox
