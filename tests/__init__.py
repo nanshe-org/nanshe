6acca76856069e88c727a4e858d1475944ac0405
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-27 23:24:23 -0400

    tests/__init__.py: Make sure all tests are imported if possible.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,31 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Jun 06, 2014 16:21:22 EDT$"
 
-__all__ = ["test_additional_io", "test_advanced_image_processing", "test_debugging_tools", "test_extended_region_props", "test_HDF5_searchers", "test_HDF5_serializers", "test_nanshe_learner", "test_registration", "test_spams_sandbox", "test_tiff_file_format", "random_dictionary_learning_data"]
+__all__ = [
+    "test_additional_io", "test_advanced_image_processing",
+    "test_debugging_tools", "test_extended_region_props",
+    "test_generic_decorators", "test_HDF5_recorder", "test_HDF5_searchers",
+    "test_HDF5_serializers", "test_nanshe_converter", "test_nanshe_learner",
+    "test_nanshe_registerer",  # "test_nanshe_viewer",
+    "test_registration", "test_spams_sandbox", "test_tiff_file_format",
+    "testPathHelpers", "random_dictionary_learning_data"
+]
 
 
 import test_additional_io
 import test_advanced_image_processing
 import test_debugging_tools
 import test_extended_region_props
+import test_generic_decorators
+import test_HDF5_recorder
 import test_HDF5_searchers
 import test_HDF5_serializers
+import test_nanshe_converter
 import test_nanshe_learner
+import test_nanshe_registerer
+# import test_nanshe_viewer
 import test_registration
 import test_spams_sandbox
 import test_tiff_file_format
+import testPathHelpers
 import random_dictionary_learning_data
