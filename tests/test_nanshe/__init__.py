e6ba5313c5f32de8971b9ffa45c78cecf6e7d758
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-29 23:07:55 -0400

    tests/test_nanshe/test_io/__init__.py: Add the new test module `test_io` to contain all tests of modules that use some sort of file IO. tests/test_nanshe/__init__.py: Add the `test_io` module to the list of imports.

--- tests/test_nanshe/__init__.py
+++ tests/test_nanshe/__init__.py
@@ -1,10 +1,13 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 27, 2015 19:27:12 EDT$"
 
-__all__ = ["test_hdf5", "test_nanshe", "test_spams_sandbox", "test_util"]
+__all__ = [
+    "test_hdf5", "test_io", "test_nanshe", "test_spams_sandbox", "test_util"
+]
 
 
 import test_hdf5
+import test_io
 import test_nanshe
 import test_spams_sandbox
 import test_util
---
+++ tests/test_nanshe/test_io/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Mar 29, 2015 23:07:38 EDT$"
+
+__all__ = []
+
+
