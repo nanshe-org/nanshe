245b0d20b4650a2e8c4349136aa4654f971d780e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-30 14:32:13 -0400

    Rename `nanshe.nanshe_viewer` to `nanshe.viewer`.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -3,16 +3,16 @@ __date__ = "$Dec 22, 2014 08:46:12 EST$"
 
 
 __all__ = [
-    "converter", "io", "imp", "learner",  # "nanshe_viewer",
-    "registerer", "spams_sandbox", "synthetic_data", "util"
+    "converter", "io", "imp", "learner" "registerer", "spams_sandbox",
+    "synthetic_data", "util",  # "viewer"
 ]
 
 import converter
 import io
 import imp
 import learner
-# import nanshe_viewer    # Must be commented as there is some segfault coming from Volumina.
 import registerer
 import spams_sandbox
 import synthetic_data
 import util
+# import viewer           # Must be commented as there is some segfault coming from Volumina.
--- tests/test_nanshe/__init__.py
+++ tests/test_nanshe/__init__.py
@@ -2,9 +2,8 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 27, 2015 19:27:12 EDT$"
 
 __all__ = [
-    "test_converter", "test_io", "test_imp", "test_learner",
-    # "test_nanshe_viewer",
-    "test_registerer", "test_spams_sandbox", "test_util"
+    "test_converter", "test_io", "test_imp", "test_learner", "test_registerer",
+    "test_spams_sandbox", "test_util",  # "test_viewer"
 ]
 
 
@@ -12,7 +11,7 @@ import test_converter
 import test_io
 import test_imp
 import test_learner
-# import test_nanshe_viewer
 import test_registerer
 import test_spams_sandbox
 import test_util
+# import test_viewer
