6fff7d44b585df89c7d221fe1e4bbe76f586a6f4
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-30 23:18:30 -0400

    setup.py: First attempt at a setup file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Mar 30, 2015 23:17:09 EDT$"
+
+from distutils.core import setup
+
+setup(
+    name="nanshe",
+    version="0.1a",
+    packages=["nanshe"],
+    url="https://github.com/jakirkham/nanshe",
+    license="GPLv3",
+    author="John Kirkham",
+    author_email="kirkhamj@janelia.hhmi.org",
+    description="An image processing toolkit."
+)
