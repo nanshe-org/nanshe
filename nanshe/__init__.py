ed89e743c2274dcc18b5f67ca4923c5ad2c20c49
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 09:21:13 -0400

    nanshe/__init__.py: Add documentation about install.

--- nanshe/__init__.py
+++ nanshe/__init__.py
@@ -15,9 +15,32 @@ applied optionally. For example, removing registration artifacts from
 a line-by-line registration algorithm, background subtraction, and a wavelet
 transform to filter objects in a particular size.
 
+Implementation of the algorithm has been done here in pure Python. However, a
+few dependencies are required to get started. These include NumPy_, SciPy_,
+h5py_, scikit-image_, SPAMS_, VIGRA_, and rank_filter_. The first 4 can be
+found in standard distributions like Anaconda_. Installing VIGRA and
+rank_filter can be done by using CMake_. SPAMS requires an existing BLAS/LAPACK
+implementation. On Mac and Linux, this can be anything. Typically ATLAS_ is
+used, but OpenBLAS_ or `Intel MKL`_ (if available) can be used, as well. This
+will require modifying the setup.py script. On Windows, the setup.py links to
+R_, which should be changed if another BLAS is available.
+
 
 .. _`10.1109/ISBI.2013.6556660`: http://dx.doi.org/10.1109/ISBI.2013.6556660
 .. _`10.1145/1553374.1553463`: http://dx.doi.org/10.1145/1553374.1553463
+.. _NumPy: http://www.numpy.org/
+.. _SciPy: http://www.scipy.org/
+.. _h5py: http://www.h5py.org/
+.. _scikit-image: http://scikit-image.org/
+.. _SPAMS: http://spams-devel.gforge.inria.fr/
+.. _VIGRA: http://ukoethe.github.io/vigra/
+.. _rank_filter: https://github.com/jakirkham/rank_filter/
+.. _Anaconda: https://store.continuum.io/cshop/anaconda/
+.. _CMake: http://www.cmake.org/
+.. _ATLAS: http://math-atlas.sourceforge.net/
+.. _OpenBLAS: http://www.openblas.net/
+.. _`Intel MKL`: https://software.intel.com/en-us/intel-mkl
+.. _R: http://www.r-project.org/
 """
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 22, 2014 08:46:12 EST$"
