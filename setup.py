6ec8d626b127add9c8fe9a1c59e5f09f3268e54e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 00:27:00 -0400

    setup.py: Include binary scripts to install.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 30, 2015 23:17:09 EDT$"
 
 
+from glob import glob
+
 from setuptools import setup, find_packages
 
 setup(
@@ -13,5 +15,6 @@ setup(
     author="John Kirkham",
     author_email="kirkhamj@janelia.hhmi.org",
     description="An image processing toolkit.",
-    setup_requires=["nose>=1.2", "sphinx>=1.3"]
+    setup_requires=["nose>=1.2", "sphinx>=1.3"],
+    scripts=glob("bin/*")
 )
