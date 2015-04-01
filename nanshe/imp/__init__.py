7456ce0d8686d0ba379bf7d06890c9b8a15129ac
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 21:00:46 -0400

    nanshe/imp/__init__.py: Drop `binary_image_processing` from the module imports.

--- nanshe/imp/__init__.py
+++ nanshe/imp/__init__.py
@@ -2,13 +2,12 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 20:37:08 EDT$"
 
 __all__ = [
-    "advanced_image_processing", "binary_image_processing", "denoising",
+    "advanced_image_processing", "denoising",
     "filters",  # "neuron_matplotlib_viewer",
     "registration", "simple_image_processing", "wavelet_transform"
 ]
 
 import advanced_image_processing
-import binary_image_processing
 import denoising
 import filters
 # import neuron_matplotlib_viewer
