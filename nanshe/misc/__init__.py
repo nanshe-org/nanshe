9a9c2e745b3c07fc7c7af1644968531c1707b86d
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-01 00:19:00 -0400

    Moved `random_dictionary_learning_data` from `tests` to `nanshe.misc`.

--- nanshe/misc/__init__.py
+++ nanshe/misc/__init__.py
@@ -2,6 +2,8 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 01, 2015 00:13:16 EDT$"
 
 __all__ = [  # "neuron_matplotlib_viewer",
+    "random_dictionary_learning_data"
 ]
 
 # import neuron_matplotlib_viewer
+import random_dictionary_learning_data
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,7 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Jun 06, 2014 16:21:22 EDT$"
 
-__all__ = [
-    "test_nanshe", "random_dictionary_learning_data"
-]
+__all__ = ["test_nanshe"]
 
 
 import test_nanshe
-import random_dictionary_learning_data
