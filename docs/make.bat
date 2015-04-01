9ecc15ce8cbc26eaa9dd11dccebb0ee0a7f91aed
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 20:31:28 -0400

    docs/make.bat: Put each module on a separate page.

--- docs/make.bat
+++ docs/make.bat
@@ -85,7 +85,7 @@ if errorlevel 9009 (
 )
 
 if "%1" == "rst" (
-        %SPHINXAPIDOC% -f -T -M -o . .. ../setup.py ../tests
+        %SPHINXAPIDOC% -f -T -e -M -o . .. ../setup.py ../tests
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished. The RST pages are in in the current directory.
