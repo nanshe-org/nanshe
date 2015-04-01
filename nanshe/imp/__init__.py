094d9a73af495a192050517c8151e88298e5a708
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-01 00:16:54 -0400

    Moved `neuron_matplotlib_viewer` from `nanshe.imp` to `nanshe.misc`.

--- nanshe/imp/__init__.py
+++ nanshe/imp/__init__.py
@@ -1,13 +1,9 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 20:37:08 EDT$"
 
-__all__ = [
-    "filters",  # "neuron_matplotlib_viewer",
-    "registration", "renorm", "segment"
-]
+__all__ = ["filters", "registration", "renorm", "segment"]
 
 import filters
-# import neuron_matplotlib_viewer
 import registration
 import renorm
 import segment
--- nanshe/misc/__init__.py
+++ nanshe/misc/__init__.py
@@ -1,5 +1,7 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 01, 2015 00:13:16 EDT$"
 
-__all__ = []
+__all__ = [  # "neuron_matplotlib_viewer",
+]
 
+# import neuron_matplotlib_viewer
