c9f0bfafde6c2c3d78fb979498ef1f235c8418b5
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 20:53:49 -0400

    nanshe/imp/filters.py: Add logging support.

--- nanshe/imp/filters.py
+++ nanshe/imp/filters.py
@@ -4,3 +4,11 @@ __date__ = "$Mar 31, 2015 20:47:37 EDT$"
 
 from rank_filter import lineRankOrderFilter
 
+# Need in order to have logging information no matter what.
+from nanshe.util import prof
+
+
+# Get the logger
+logger = prof.logging.getLogger(__name__)
+
+
