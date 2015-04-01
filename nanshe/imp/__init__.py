381c75be231cdc3338596df50d81970ffe8fb322
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 20:50:02 -0400

    nanshe/imp/__init__.py: Add `filters` to the list of imports.

--- nanshe/imp/__init__.py
+++ nanshe/imp/__init__.py
@@ -3,13 +3,14 @@ __date__ = "$Apr 14, 2014 20:37:08 EDT$"
 
 __all__ = [
     "advanced_image_processing", "binary_image_processing", "denoising",
-    # "neuron_matplotlib_viewer",
+    "filters",  # "neuron_matplotlib_viewer",
     "registration", "simple_image_processing", "wavelet_transform"
 ]
 
 import advanced_image_processing
 import binary_image_processing
 import denoising
+import filters
 # import neuron_matplotlib_viewer
 import registration
 import simple_image_processing
