6556a18cee77b04eb0f03e3365c1705f5ddebde8
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-31 22:28:29 -0400

    Rename filters to masks.

--- nanshe/imp/__init__.py
+++ nanshe/imp/__init__.py
@@ -2,14 +2,14 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 14, 2014 20:37:08 EDT$"
 
 __all__ = [
-    "advanced_image_processing", "denoising",
-    "filters",  # "neuron_matplotlib_viewer",
+    "advanced_image_processing", "denoising", "masks",
+    # "neuron_matplotlib_viewer",
     "registration", "simple_image_processing", "wavelet_transform"
 ]
 
 import advanced_image_processing
 import denoising
-import filters
+import masks
 # import neuron_matplotlib_viewer
 import registration
 import simple_image_processing
