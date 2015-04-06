a3d57051dc67ef0ba411a3b92ddcf2b5b0ac4b1a
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-06 09:39:05 -0400

    nanshe/util/prof.py: Added `getTraceLogger`, which acts like `logging.getLogger`, but changes the logger name by prepending `TRACE`.

--- nanshe/util/prof.py
+++ nanshe/util/prof.py
@@ -48,6 +48,30 @@ def getSpecialLogger(logger_prefix, name, *args, **kwargs):
     )
 
 
+def getTraceLogger(name, *args, **kwargs):
+    """
+        A fancy version of ``logging.getLogger``, which adds the prefix TRACE
+        to the name given.
+
+        Args:
+            name(str):                  The name of the function or module
+                                        being logged.
+
+            *args:                      Other arguments to pass through to
+                                        ``getLogger``. Currently, it takes no
+                                        others.
+
+            *kwargs:                    Other keyword arguments to pass through
+                                        to ``getLogger``. Currently, it takes
+                                        no others.
+
+        Returns:
+            logging.Logger:             A logger with the given name.
+    """
+
+    return(getSpecialLogger("TRACE", name, *args, **kwargs))
+
+
 def log_call(logger, to_log_call = True, to_print_args = False, to_print_time = True, to_print_exception = False):
     """
         Takes a given logger and uses it to log entering and leaving the decorated callable.
