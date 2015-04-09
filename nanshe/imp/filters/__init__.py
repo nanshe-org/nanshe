33aceb494cdd9b8272b916714705b851dbdce248
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:38:07 -0400

    nanshe/imp/filters/__init__.py: Add a summary and brief description of the package.

--- nanshe/imp/filters/__init__.py
+++ nanshe/imp/filters/__init__.py
@@ -1,3 +1,16 @@
+"""
+The package ``filter`` provides a variety of transformations for image data.
+
+===============================================================================
+Overview
+===============================================================================
+Provides a variety of different filters for data analysis. This includes
+different types of convolutions or other related transformations. Operations on
+masks are supported. Computations and removal of noise are also included.
+Finally, support for performing wavelet transforms is provided.
+"""
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 31, 2015 22:29:31 EDT$"
 
