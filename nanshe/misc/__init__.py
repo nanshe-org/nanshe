d7d8be9ec2b0652c360a69cf404671766916ba05
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:49:09 -0400

    nanshe/misc/__init__.py: Add a summary and brief description of the package.

--- nanshe/misc/__init__.py
+++ nanshe/misc/__init__.py
@@ -1,3 +1,17 @@
+"""
+The package ``misc`` contains everything that does not really belong elsewhere.
+
+===============================================================================
+Overview
+===============================================================================
+The package ``misc`` (short for **misc**\ ellaneous) contains random components
+that did not really belong anywhere else. This stuff is likely not well
+documented or tested. As a consequence, it is safe to assume that this stuff
+is not stable and may be deprecated. If does ever leave this package (and is
+not deleted), its API may be broken from what it was here.
+"""
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 01, 2015 00:13:16 EDT$"
 
