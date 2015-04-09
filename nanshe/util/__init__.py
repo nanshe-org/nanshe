90bced59286bfbee03d34edadfc91c0156051a07
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:52:51 -0400

    nanshe/util/__init__.py: Add a summary and brief description of the package.

--- nanshe/util/__init__.py
+++ nanshe/util/__init__.py
@@ -1,3 +1,16 @@
+"""
+The package ``util`` contains a collection of basic low level functions.
+
+===============================================================================
+Overview
+===============================================================================
+The package ``util`` (short for **util**\ ities) contains a collection of basic
+low level functions that are useful in completing in simple, common objectives
+in the library. These range from working with generators, parsing paths,
+logging/profiling, and working with arrays.
+"""
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 27, 2015 09:28:53 EDT$"
 
