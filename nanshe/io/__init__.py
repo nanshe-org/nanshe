7d1d0272713c08daac5b668da0c6829ca49f8876
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:44:10 -0400

    nanshe/io/__init__.py: Add a summary and brief description of the package.

--- nanshe/io/__init__.py
+++ nanshe/io/__init__.py
@@ -1,3 +1,15 @@
+"""
+The package ``io`` provides support for various IO operations.
+
+===============================================================================
+Overview
+===============================================================================
+The package ``io`` contains a variety helper functions and methods to address
+IO operations. Particular focus is given to HDF5. Support is also provided in
+using JSON as a configuration format and conversion of TIFF to HDF5.
+"""
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 29, 2015 23:03:14 EDT$"
 
