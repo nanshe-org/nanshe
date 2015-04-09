916304230963542db315fc067e9b846017d0a1c8
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:44:36 -0400

    nanshe/io/hdf5/__init__.py: Add a summary and brief description of the package.

--- nanshe/io/hdf5/__init__.py
+++ nanshe/io/hdf5/__init__.py
@@ -1,3 +1,18 @@
+"""
+The package ``hdf5`` provides support for IO operations on HDF5 files.
+
+===============================================================================
+Overview
+===============================================================================
+The package ``hdf5`` contains a variety helper functions and methods to address
+IO operations on HDF5 files. Using this package requires a working copy of
+|h5py|_ on your ``PYTHONPATH``.
+
+.. |h5py| replace:: ``h5py``
+.. _h5py: http://www.h5py.org/
+"""
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Mar 27, 2015 09:32:02 EDT$"
 
