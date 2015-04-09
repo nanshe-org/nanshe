bb11e39d86f0c872ad86257ca793737f168792bd
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:52:03 -0400

    nanshe/syn/__init__.py: Add a summary and brief description of the package.

--- nanshe/syn/__init__.py
+++ nanshe/syn/__init__.py
@@ -1,3 +1,16 @@
+"""
+The package ``syn`` provides components necessary to generate synthetic data.
+
+===============================================================================
+Overview
+===============================================================================
+The package ``syn`` (short for **syn**\ thetic) contains components necessary
+for generating synthetic data. This is primarily used for testing the
+algorithm. However, it could be used for benchmarking algorithms or in other
+cases.
+"""
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Oct 15, 2014 18:16:33 EDT$"
 
