de2ed3bdd12d0d3e8bad7fa880ae035635a0698f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-09 10:50:46 -0400

    nanshe/misc/neuron_matplotlib_viewer.py: Add a summary and brief description of the module.

--- nanshe/misc/neuron_matplotlib_viewer.py
+++ nanshe/misc/neuron_matplotlib_viewer.py
@@ -1,4 +1,16 @@
 """
+The ``neuron_matplotlib_viewer`` module provides a ``matplotlib``-based viewer.
+
+===============================================================================
+Overview
+===============================================================================
+The module ``neuron_matplotlib_viewer`` provides a simple |matplotlib|_ viewer
+for navigating through a 3D imagestack (TYX). However the first dimension could
+be Z, as well. This has been deprecated in favor of the ``viewer``.
+
+.. |matplotlib| replace:: ``matplotlib``
+.. _matplotlib: http://matplotlib.org
+
 ===============================================================================
 API
 ===============================================================================
