a21cd35b2f7f8a671ac4d9a31e8fc737fcab7631
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-10 12:31:39 -0400

    nanshe/registerer.py: Wrap lines.

--- nanshe/registerer.py
+++ nanshe/registerer.py
@@ -49,34 +49,44 @@ def main(*argv):
             int:            exit code (0 if success)
     """
 
-    # Only necessary if running main (normally if calling command line). No point in importing otherwise.
+    # Only necessary if running main (normally if calling command line). No
+    # point in importing otherwise.
     import argparse
 
     argv = list(argv)
 
     # Creates command line parser
-    parser = argparse.ArgumentParser(description="Parses input from the command line for a registration job.")
+    parser = argparse.ArgumentParser(
+        description="Parses input from the command line " +
+                    "for a registration job."
+    )
 
     parser.add_argument("config_filename",
                         metavar="CONFIG_FILE",
                         type=str,
-                        help="JSON file that provides configuration options for how to import TIFF(s)."
+                        help="JSON file that provides configuration options " +
+                             "for how to import TIFF(s)."
     )
     parser.add_argument("input_filenames",
                         metavar="INPUT_FILE",
                         type=str,
                         nargs="+",
-                        help="HDF5 file to import (this should include a path to where the internal dataset should be stored)."
+                        help="HDF5 file to import (this should include a " +
+                             "path to where the internal dataset should be " +
+                             "stored)."
     )
 
     parser.add_argument("output_filenames",
                         metavar="OUTPUT_FILE",
                         type=str,
                         nargs=1,
-                        help="HDF5 file to export (this should include a path to where the internal dataset should be stored)."
+                        help="HDF5 file to export (this should include a " +
+                             "path to where the internal dataset should be " +
+                             "stored)."
     )
 
-    # Results of parsing arguments (ignore the first one as it is the command line call).
+    # Results of parsing arguments
+    # (ignore the first one as it is the command line call).
     parsed_args = parser.parse_args(argv[1:])
 
     # Go ahead and stuff in parameters with the other parsed_args
@@ -84,14 +94,19 @@ def main(*argv):
 
     parsed_args.input_file_components = []
     for each_input_filename in parsed_args.input_filenames:
-        parsed_args.input_file_components.append(PathComponents(each_input_filename))
+        parsed_args.input_file_components.append(
+            PathComponents(each_input_filename)
+        )
 
     parsed_args.output_file_components = []
     for each_output_filename in parsed_args.output_filenames:
-        parsed_args.output_file_components.append(PathComponents(each_output_filename))
+        parsed_args.output_file_components.append(
+            PathComponents(each_output_filename)
+        )
 
 
-    for each_input_filename_components, each_output_filename_components in itertools.izip(parsed_args.input_file_components, parsed_args.output_file_components):
+    for each_input_filename_components, each_output_filename_components in itertools.izip(
+            parsed_args.input_file_components, parsed_args.output_file_components):
         with h5py.File(each_input_filename_components.externalPath, "r") as input_file:
             with h5py.File(each_output_filename_components.externalPath, "a") as output_file:
                 data = input_file[each_input_filename_components.internalPath]
