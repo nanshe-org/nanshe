ef41c4c307db1d8a25805b435df42fe00d0a4dff
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-12 23:40:19 -0400

    nanshe/util/prof.py: Wrap docstrings.

--- nanshe/util/prof.py
+++ nanshe/util/prof.py
@@ -122,27 +122,42 @@ def getTraceMetaLogger(name, *args, **kwargs):
 
 def log_call(logger, to_log_call=True, to_print_args=False, to_print_time=True, to_print_exception=False):
     """
-        Takes a given logger and uses it to log entering and leaving the decorated callable.
-        Intended to be used as a decorator that takes a few arguments.
+        Takes a given logger and uses it to log entering and leaving the
+        decorated callable. Intended to be used as a decorator that takes a few
+        arguments.
 
         Args:
-            logger      (Logger):          Used for logging entry, exit and possibly arguments.
+            logger(Logger):                Used for logging entry, exit and
+                                           possibly arguments.
 
         Keyword Args:
-            to_log_call (bool):            Whether to log call or not. This overrides all other arguments. It will be
-                                           stored as a global variable on the function, which can be changed at runtime.
+            to_log_call(bool):             Whether to log call or not. This
+                                           overrides all other arguments. It
+                                           will be stored as a global variable
+                                           on the function, which can be
+                                           changed at runtime.
 
-            to_print_args  (bool):         Whether to output the arguments and keyword arguments passed to the function.
-                                           This should not automatically be true as some arguments may not be printable
-                                           or may be expensive to print. Thus, it should be up to the developer to use
-                                           their own discretion. Further, we don't want to break their existing code. It
-                                           will be stored as a global variable on the function, which can be changed at
+            to_print_args(bool):           Whether to output the arguments and
+                                           keyword arguments passed to the
+                                           function. This should not
+                                           automatically be true as some
+                                           arguments may not be printable or
+                                           may be expensive to print. Thus, it
+                                           should be up to the developer to use
+                                           their own discretion. Further, we
+                                           don't want to break their existing
+                                           code. It will be stored as a global
+                                           variable on the function, which can
+                                           be changed at runtime.
+
+            to_print_time(bool):           Prints the time it took to run the
+                                           wrapped callable.
+
+            to_print_exception  (bool):    Whether to print the traceback when
+                                           an exception is raise. It will be
+                                           stored as a global variable on the
+                                           function, which can be changed at
                                            runtime.
-
-            to_print_time(bool):           Prints the time it took to run the wrapped callable.
-
-            to_print_exception  (bool):    Whether to print the traceback when an exception is raise. It will be stored
-                                           as a global variable on the function, which can be changed at runtime.
 
         Returns:
             log_call_decorator (for wrapping)
@@ -150,13 +165,18 @@ def log_call(logger, to_log_call=True, to_print_args=False, to_print_time=True, 
 
     def log_call_decorator(callable):
         """
-            The actual decorator, which is what is returned to decorate the callable in question.
+            The actual decorator, which is what is returned to decorate the
+            callable in question.
 
             Args:
-                callable:    Anything that can be called function, method, functor, lambda, etc.
+                callable:                              Anything that can be
+                                                       called function, method,
+                                                       functor, lambda, etc.
 
             Returns:
-                log_call_callable_wrapped, which is wrapped around the function in question.
+                log_call_callable_wrapped(callable):   which is wrapped around
+                                                       the function in
+                                                       question.
         """
 
         @wrappers.wraps(callable)
@@ -166,14 +186,20 @@ def log_call(logger, to_log_call=True, to_print_args=False, to_print_time=True, 
                                    to_print_exception=to_print_exception)
         def log_call_callable_wrapped(*args, **kwargs):
             """
-                This is what will replace the original callable. It will behave the same except it will now log its
-                entrance and exit. If set, it will log its parameters too.
+                This is what will replace the original callable. It will behave
+                the same except it will now log its entrance and exit. If set,
+                it will log its parameters too.
 
                 Args:
-                    callable:    Anything that can be called function, method, functor, lambda, etc.
+                    callable:                               Anything that can
+                                                            be called function,
+                                                            method, functor,
+                                                            lambda, etc.
 
                 Returns:
-                    log_call_callable_wrapped, which is wrapped around the function in question.
+                    log_call_callable_wrapped (callable):   which is wrapped
+                                                            around the function
+                                                            in question.
             """
 
             result = None
@@ -240,30 +266,45 @@ def log_class(logger,
               to_print_time=True,
               to_print_exception=False):
     """
-        Takes a given logger and uses it to log entering and leaving all methods of the decorated class.
-        Intended to be used as a decorator that takes a few arguments.
+        Takes a given logger and uses it to log entering and leaving all
+        methods of the decorated class. Intended to be used as a decorator that
+        takes a few arguments.
 
         Args:
-            logger      (Logger):          Used for logging entry, exit and possibly arguments.
+            logger(Logger):                Used for logging entry, exit and
+                                           possibly arguments.
 
         Keyword Args:
-            to_log_call (bool):            Whether to log call or not. This overrides all other arguments. It will be
-                                           stored as a global variable on the methods, which can be changed at runtime.
+            to_log_call(bool):             Whether to log call or not. This
+                                           overrides all other arguments. It
+                                           will be stored as a global variable
+                                           on the methods, which can be changed
+                                           at runtime.
 
-            to_print_args  (bool):         Whether to output the arguments and keyword arguments passed to the function.
-                                           This should not automatically be true as some arguments may not be printable
-                                           or may be expensive to print. Thus, it should be up to the developer to use
-                                           their own discretion. Further, we don't want to break their existing code. It
-                                           will be stored as a global variable on the methods, which can be changed at
+            to_print_args(bool):           Whether to output the arguments and
+                                           keyword arguments passed to the
+                                           function. This should not
+                                           automatically be true as some
+                                           arguments may not be printable or
+                                           may be expensive to print. Thus, it
+                                           should be up to the developer to use
+                                           their own discretion. Further, we
+                                           don't want to break their existing
+                                           code. It will be stored as a global
+                                           variable on the methods, which can
+                                           be changed at runtime.
+
+            to_print_time(bool):           Prints the time it took to run the
+                                           wrapped callable.
+
+            to_print_exception(bool):      Whether to print the traceback when
+                                           an exception is raise. It will be
+                                           stored as a global variable on the
+                                           methods, which can be changed at
                                            runtime.
 
-            to_print_time(bool):           Prints the time it took to run the wrapped callable.
-
-            to_print_exception  (bool):    Whether to print the traceback when an exception is raise. It will be stored
-                                           as a global variable on the methods, which can be changed at runtime.
-
         Returns:
-            log_call_decorator (for wrapping)
+            log_call_decorator(for wrapping)
     """
 
     return(wrappers.class_decorate_all_methods(log_call(
@@ -277,30 +318,45 @@ def log_class(logger,
 
 def qt_log_class(logger, to_log_call=True, to_print_args=False, to_print_time=True, to_print_exception=False):
     """
-        Takes a given logger and uses it to log entering and leaving all methods of the decorated class.
-        Intended to be used as a decorator that takes a few arguments.
+        Takes a given logger and uses it to log entering and leaving all
+        methods of the decorated class. Intended to be used as a decorator that
+        takes a few arguments.
 
         Args:
-            logger      (Logger):          Used for logging entry, exit and possibly arguments.
+            logger(Logger):                Used for logging entry, exit and
+                                           possibly arguments.
 
         Keyword Args:
-            to_log_call (bool):            Whether to log call or not. This overrides all other arguments. It will be
-                                           stored as a global variable on the methods, which can be changed at runtime.
+            to_log_call(bool):             Whether to log call or not. This
+                                           overrides all other arguments. It
+                                           will be stored as a global variable
+                                           on the methods, which can be changed
+                                           at runtime.
 
-            to_print_args  (bool):         Whether to output the arguments and keyword arguments passed to the function.
-                                           This should not automatically be true as some arguments may not be printable
-                                           or may be expensive to print. Thus, it should be up to the developer to use
-                                           their own discretion. Further, we don't want to break their existing code. It
-                                           will be stored as a global variable on the methods, which can be changed at
+            to_print_args(bool):           Whether to output the arguments and
+                                           keyword arguments passed to the
+                                           function. This should not
+                                           automatically be true as some
+                                           arguments may not be printable or
+                                           may be expensive to print. Thus, it
+                                           should be up to the developer to use
+                                           their own discretion. Further, we
+                                           don't want to break their existing
+                                           code. It will be stored as a global
+                                           variable on the methods, which can
+                                           be changed at runtime.
+
+            to_print_time(bool):           Prints the time it took to run the
+                                           wrapped callable.
+
+            to_print_exception(bool):      Whether to print the traceback when
+                                           an exception is raise. It will be
+                                           stored as a global variable on the
+                                           methods, which can be changed at
                                            runtime.
 
-            to_print_time(bool):           Prints the time it took to run the wrapped callable.
-
-            to_print_exception  (bool):    Whether to print the traceback when an exception is raise. It will be stored
-                                           as a global variable on the methods, which can be changed at runtime.
-
         Returns:
-            log_call_decorator (for wrapping)
+            log_call_decorator(for wrapping)
     """
 
     return(wrappers.qt_class_decorate_all_methods(log_call(
@@ -315,13 +371,16 @@ def qt_log_class(logger, to_log_call=True, to_print_args=False, to_print_time=Tr
 @wrappers.static_variables(to_run=True)
 def memory_profiler(logger, interval=1, level=logging.INFO):
     """
-        Runs forever get information about memory usage and dumping it to the logger provided at the given interval.
+        Runs forever get information about memory usage and dumping it to the
+        logger provided at the given interval.
 
         Args:
-            logger (Logger):               Used for logging memory profiling information.
+            logger(Logger):               Used for logging memory profiling
+                                          information.
 
         Keyword Args:
-            interval (int or float):       Number of seconds to wait before issuing more profile information.
+            interval(int or float):       Number of seconds to wait before
+                                          issuing more profile information.
     """
 
     current_process = psutil.Process(os.getpid())
