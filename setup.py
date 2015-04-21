6c0fc651e50bcf855e003de664e582ec564e2fb9
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-20 21:14:05 -0400

    setup.py: Specify requirements depending on whether a conda package is being built or not.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ __date__ = "$Mar 30, 2015 23:17:09 EDT$"
 
 
 from glob import glob
+import sys
 
 from setuptools import setup, find_packages
 import versioneer
@@ -14,6 +15,49 @@ versioneer.versionfile_build = None
 versioneer.tag_prefix = "v"
 versioneer.parentdir_prefix = "nanshe-"
 
+build_requires = []
+install_requires = []
+tests_require = []
+if sys.argv[1] == "bdist_conda":
+    build_requires = [
+        "openblas",
+        "fftw",
+        "setuptools",
+        "psutil",
+        "numpy",
+        "scipy",
+        "h5py",
+        "bottleneck",
+        "pyfftw",
+        "scikit-image",
+        "vigra",
+        "spams",
+        "rank_filter",
+        "pyqt",
+        "volumina"
+    ]
+
+    install_requires = [
+        "openblas",
+        "fftw",
+        "setuptools",
+        "psutil",
+        "numpy",
+        "scipy",
+        "h5py",
+        "bottleneck",
+        "pyfftw",
+        "scikit-image",
+        "vigra",
+        "spams",
+        "rank_filter",
+        "pyqt",
+        "volumina"
+    ]
+
+    tests_require = [
+        "nose"
+    ]
 
 setup(
     name="nanshe",
@@ -26,5 +70,8 @@ setup(
     scripts=glob("bin/*"),
     packages=find_packages(exclude=["tests*"]),
     cmdclass=versioneer.get_cmdclass(),
+    build_requires=build_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
     zip_safe=True
 )
