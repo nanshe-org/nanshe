04b26373a116d66c4737a5f314ac0bcf55c548a5
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-29 11:30:49 -0400

    docs/make.bat: Make sure the TODO list is saved on `make.bat clean`.

--- docs/make.bat
+++ docs/make.bat
@@ -49,9 +49,11 @@ if "%1" == "help" (
 if "%1" == "clean" (
 	move index.rst index.rst.saved
 	move readme.rst readme.rst.saved
+	move todo.rst todo.rst.saved
 	del /q /s *.rst
 	move index.rst.saved index.rst
 	move readme.rst.saved readme.rst
+	move todo.rst.saved todo.rst
 	for /d %%i in (%BUILDDIR%\*) do rmdir /q /s %%i
 	del /q /s %BUILDDIR%
 	goto end
