0e4afd6566c00883da18348157116980e4b9a00d
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-11-04 09:56:11 -0500

    nanshe/util/xnumpy.py: Allow `-1` for any dimension of block shape and if it is found replace it by the full shape length.

--- nanshe/util/xnumpy.py
+++ nanshe/util/xnumpy.py
@@ -3091,6 +3091,9 @@ def blocks_split(space_shape, block_shape, block_halo=None):
 
     for each_dim in xrange(len(space_shape)):
         # Construct each block using the block size given. Allow to spill over.
+        if block_shape[each_dim] == -1:
+            block_shape[each_dim] = space_shape[each_dim]
+
         a_range = numpy.arange(0, space_shape[each_dim], block_shape[each_dim])
         a_range = expand_view(a_range, reps_before=2).copy()
         a_range[1] += block_shape[each_dim]
