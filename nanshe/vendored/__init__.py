4544f5630f2d79f663109d644f5176581def5008
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-09-06 17:35:22 -0400

    Prep for vendoring of dependencies.

---
+++ nanshe/vendored/__init__.py
@@ -0,0 +1,17 @@
+"""
+This package exists to provided vendored copies of libraries.
+
+===============================================================================
+Overview
+===============================================================================
+The ``vendored`` package exists to hold git submodules for various dependencies
+that have since been refactored out of this library or written from scratch and
+added as dependencies. This is not intended to exist for the long term.
+"""
+
+
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Sep 06, 2016 09:57:10 EDT$"
+
+__all__ = [
+]
