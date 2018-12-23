245df4f52bc8b51a9994a418db5e4b66922b8c8c
Travis CI <noreply@travis-ci.org>
2018-12-23 02:40:39 +0000

    Rebuilt documentation for commit (bc6807c96dc0098809b56eb592cb555cbf2fda2a).

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a63+2.g94eb320',
+    VERSION: '0.1.0a63+4.gbc6807c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
